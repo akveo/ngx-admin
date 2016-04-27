d5dab605a9f38239614134492b40b7b8ef56d3d8
smartapant <smartapant@gmail.com>
2016-04-27 18:21:52 +0300

    Change pageTop bg onScroll.

---
+++ src/app/theme/directives/scrollPosition.directive.ts
@@ -0,0 +1,23 @@
+import {Directive, Input, Output, EventEmitter, HostListener, ElementRef} from 'angular2/core';
+@Directive({
+    selector: '[scrollPosition]'
+})
+export class ScrollPosition {
+    @Input() maxHeight: Number;
+    @Output() scrollChange:EventEmitter<Boolean> = new EventEmitter<Boolean>();
+
+    private _isScrolled: Boolean;
+
+    ngOnInit() {
+        this.onWindowScroll();
+    }
+
+    @HostListener('window:scroll')
+    onWindowScroll() : void {
+        let isScrolled = window.scrollY > this.maxHeight;
+        if (isScrolled !== this._isScrolled) {
+            this._isScrolled = isScrolled;
+            this.scrollChange.emit(isScrolled);
+        }
+    }
+}
