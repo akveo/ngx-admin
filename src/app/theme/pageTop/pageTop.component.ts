ce4671f81f6d6e7000288890cfdb5bd177bef394
nixa <4dmitr@gmail.com>
2016-04-27 11:06:16 +0300

    main user profile avatar fix

--- src/app/theme/pageTop/pageTop.component.ts
+++ src/app/theme/pageTop/pageTop.component.ts
@@ -1,11 +1,13 @@
 import {Component, ViewEncapsulation} from 'angular2/core';
 
 import {MsgCenter} from '../msgCenter';
+import {ProfilePicturePipe} from '../pipes/image/profile-picture.pipe';
 
 @Component({
     selector: 'page-top',
     styles: [ require('./pageTop.scss') ],
     template: require('./pageTop.html'),
-    directives: [MsgCenter]
+    directives: [MsgCenter],
+    pipes: [ProfilePicturePipe]
 })
 export class PageTop {}
