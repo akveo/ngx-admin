f721a044a190ca570ab47f068254bea9716f978b
nixa <4dmitr@gmail.com>
2016-05-04 17:18:09 +0300

    traffic chart component

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,13 +2,13 @@ import {Component, ViewEncapsulation} from 'angular2/core';
 
 import {PopularApp} from './popularApp';
 import {PieChart} from './pieChart';
+import {TrafficChart} from './trafficChart';
 import {BaCard} from '../../theme/components';
-
 
 @Component({
   selector: 'dashboard',
   pipes: [],
-  directives: [PopularApp, PieChart, BaCard],
+  directives: [PopularApp, PieChart, TrafficChart, BaCard],
   encapsulation: ViewEncapsulation.None,
   styles: [require('./dashboard.scss')],
   template: require('./dashboard.html')
