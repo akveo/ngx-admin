20f81773c332ae9869bb15686ed8fe4389dbdc50
nixa <4dmitr@gmail.com>
2016-05-06 12:30:50 +0300

    popular app component completion

--- src/app/pages/dashboard/popularApp/popularApp.component.ts
+++ src/app/pages/dashboard/popularApp/popularApp.component.ts
@@ -1,18 +1,16 @@
 import {Component, ViewEncapsulation} from 'angular2/core';
 
+import {AppPicturePipe} from '../../../theme/pipes';
+
 @Component({
   selector: 'popular-app',
   encapsulation: ViewEncapsulation.None,
+  pipes: [AppPicturePipe],
   styles: [require('./popularApp.scss')],
   template: require('./popularApp.html')
 })
 export class PopularApp {
 
-  constructor() {
-  }
-
   ngOnInit() {
-    console.log('DASHBOARD');
   }
-
 }
