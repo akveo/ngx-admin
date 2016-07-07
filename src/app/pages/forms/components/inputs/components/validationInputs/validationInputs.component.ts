721880444000f81ea2537a9302488db04cc2b600
tibing <parapupum@gmail.com>
2016-07-07 15:25:08 +0300

    feat(ba-checkbox): create checkbox component

--- src/app/pages/forms/components/inputs/components/validationInputs/validationInputs.component.ts
+++ src/app/pages/forms/components/inputs/components/validationInputs/validationInputs.component.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
+import {BaCheckbox} from '../../../../../../theme/components/baCheckbox';
 
 @Component({
   selector: 'validation-inputs',
+  directives: [BaCheckbox],
   template: require('./validationInputs.html'),
 })
 export class ValidationInputs {
