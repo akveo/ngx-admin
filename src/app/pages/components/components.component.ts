4e5d682ae84b795e74a7c5ed976e6c1c8b971408
nixa <4dmitr@gmail.com>
2016-07-12 12:15:25 +0300

    fix(merge): merge tree view component

--- src/app/pages/components/components.component.ts
+++ src/app/pages/components/components.component.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {RouteConfig} from '@angular/router-deprecated';
-import {TreeView} from "./components/treeView";
 
 @Component({
   selector: 'components',
@@ -9,14 +7,6 @@ import {TreeView} from "./components/treeView";
   styles: [],
   template: `<router-outlet></router-outlet>`
 })
-@RouteConfig([
-  {
-    name: 'TreeView',
-    component: TreeView,
-    path: '/tree-view',
-    useAsDefault: true
-  }
-])
 export class Components {
 
   constructor() {
