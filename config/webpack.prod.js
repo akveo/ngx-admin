44fe93a0c618c789951cf225164eac353bfd6a10
nixa <4dmitr@gmail.com>
2016-08-29 16:57:12 +0300

    chore(build): fix prod build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -157,7 +157,7 @@ module.exports = webpackMerge(commonConfig, {
 
 
       beautify: false, //prod
-      mangle: { screw_ie8 : true }, //prod
+      mangle: false,
       compress: { screw_ie8: true }, //prod
       comments: false //prod
     }),
