cfe658fd928d9071423a86cd2a864cd8256891b6
hoswey <hongshuwei@gmail.com>
2016-09-08 13:48:15 +0800

    no message

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -22,7 +22,7 @@ const CompressionPlugin = require('compression-webpack-plugin');
  */
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const METADATA = webpackMerge(commonConfig.metadata, {
   host: HOST,
   port: PORT,
