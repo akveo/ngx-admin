c62a66b052ca8292e7791c1ec986f86df482d3b2
hoswey <hongshuwei@gmail.com>
2016-09-18 14:22:25 +0800

    fix floating point

--- src/app/shared/pipes/format/num-to-percent.pipe.ts
+++ src/app/shared/pipes/format/num-to-percent.pipe.ts
@@ -1,11 +1,14 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
+/**
+ * 转换小数为百分数
+ */
 @Pipe({name: 'numToPercent'})
 export class NumToPercentPipe implements PipeTransform {
 
   transform(input:string, decimals?:number = 0):string {
 
     const p:number = Math.pow(10, decimals + 2);
-    return Math.round(input * p) / p * 100 + '%';
+    return (Math.round(input * p) / p * 100).toFixed(decimals) + '%';
   }
 }
