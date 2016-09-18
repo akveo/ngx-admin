53dc5740ed11fa1d9702c8702b9ebc5da5af7642
hoswey <hongshuwei@gmail.com>
2016-09-18 14:27:05 +0800

    remove footer

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -12,18 +12,18 @@ import {Component, ViewEncapsulation} from '@angular/core';
         <router-outlet></router-outlet>
       </div>
     </div>
-    <footer class="al-footer clearfix">
-      <div class="al-footer-right">Created with <i class="ion-heart"></i></div>
-      <div class="al-footer-main clearfix">
-        <div class="al-copy">&copy; <a href="http://akveo.com">Akveo</a> 2016</div>
-        <ul class="al-share clearfix">
-          <li><i class="socicon socicon-facebook"></i></li>
-          <li><i class="socicon socicon-twitter"></i></li>
-          <li><i class="socicon socicon-google"></i></li>
-          <li><i class="socicon socicon-github"></i></li>
-        </ul>
-      </div>
-    </footer>
+    <!--<footer class="al-footer clearfix">--> <!-- Hoswey, use custom footer instead-->
+      <!--<div class="al-footer-right">Created with <i class="ion-heart"></i></div>-->
+      <!--<div class="al-footer-main clearfix">-->
+        <!--<div class="al-copy">&copy; <a href="http://akveo.com">Akveo</a> 2016</div>-->
+        <!--<ul class="al-share clearfix">-->
+          <!--<li><i class="socicon socicon-facebook"></i></li>-->
+          <!--<li><i class="socicon socicon-twitter"></i></li>-->
+          <!--<li><i class="socicon socicon-google"></i></li>-->
+          <!--<li><i class="socicon socicon-github"></i></li>-->
+        <!--</ul>-->
+      <!--</div>-->
+    <!--</footer>-->
     <ba-back-top position="200"></ba-back-top>
     `
 })
