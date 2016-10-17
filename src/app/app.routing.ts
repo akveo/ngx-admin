92469d2f12eb8c95b9db8d9ce89010f920a8bc27
tibing <parapupum@gmail.com>
2016-10-17 11:54:49 +0300

    fix(app-routing): add 404 route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'pages', pathMatch: 'full' }
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pages/dashboard' }
 ];
 
 export const routing = RouterModule.forRoot(routes, { useHash: true });
