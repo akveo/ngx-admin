cdbfcb65ca234381076e60cd1f9c208f6f695cdd
yari <yarlaz@gmail.com>
2016-11-22 10:35:48 +0100

    fixed code style discrepancies

--- src/app/pages/charts/components/D3Js/D3Js.component.ts
+++ src/app/pages/charts/components/D3Js/D3Js.component.ts
@@ -1,25 +1,24 @@
 import {
-    Component,
-    OnInit
+  Component,
+  OnInit
 } from '@angular/core';
 
 import {D3JsService} from './D3Js.service';
 
 @Component({
-    selector: 'd3-js',
-    template: require('./D3Js.html'),
-    styles: [require('./D3Js.scss')],
-    providers: []
+  selector: 'd3-js',
+  template: require('./D3Js.html'),
+  styles: [require('./D3Js.scss')]
 })
 export class D3Js implements OnInit {
-    
-    data:any;
 
-    constructor(private _d3JsService:D3JsService) {
-    }
+  data: any;
 
-    ngOnInit() {
-        // get sample data
-        this.data = this._d3JsService.getAll();
-    }
+  constructor(private _d3JsService:D3JsService) {
+  }
+
+  ngOnInit() {
+    // get sample data
+    this.data = this._d3JsService.getAll();
+  }
 }
--- src/app/theme/components/baD3Chart/baD3Chart.component.ts
+++ src/app/theme/components/baD3Chart/baD3Chart.component.ts
@@ -1,12 +1,12 @@
 import {
-    Component,
-    ViewChild,
-    ViewEncapsulation,
-    Input,
-    Output,
-    EventEmitter,
-    AfterViewInit,
-    ElementRef
+  Component,
+  ViewChild,
+  ViewEncapsulation,
+  Input,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+  ElementRef
 } from '@angular/core';
 import {Subscription} from 'rxjs/Subscription';
 
@@ -14,38 +14,38 @@ import {BaD3ChartService} from './baD3Chart.service';
 import {d3} from './baD3Chart.loader';
 
 @Component({
-    encapsulation: ViewEncapsulation.None,
-    selector: 'ba-d3-chart',
-    template: require('./baD3Chart.html'),
-    styles: [require('./baD3Chart.scss')],
-    providers: [BaD3ChartService]
+  encapsulation: ViewEncapsulation.None,
+  selector: 'ba-d3-chart',
+  template: require('./baD3Chart.html'),
+  styles: [require('./baD3Chart.scss')],
+  providers: [BaD3ChartService]
 })
 export class BaD3Chart {
 
-    @Input()  baD3ChartType:string;
-    @Input()  baD3ChartData:any;
-    @Output() onChartReady = new EventEmitter<any>();
+  @Input() baD3ChartType:string;
+  @Input() baD3ChartData:any;
+  @Output() onChartReady = new EventEmitter<any>();
 
-    @ViewChild('baD3Chart') private _selector:ElementRef;
+  @ViewChild('baD3Chart') private _selector:ElementRef;
 
-    resizeCheck:Subscription;
+  resizeCheck:Subscription;
 
-    constructor(private _chartService:BaD3ChartService) {
-    }
+  constructor(private _chartService:BaD3ChartService) {
+  }
 
-    ngAfterViewInit():void {
-        // draw method returns an Observable that listens for window resize
-        // useful to unsubscribe when necessary
-        this.resizeCheck = this._chartService.drawChartNew({
-            chartType: this.baD3ChartType,
-            element: this._selector.nativeElement,
-            inputData: this.baD3ChartData
-        });
-    }
+  ngAfterViewInit() {
+    // draw method returns an Observable that listens for window resize
+    // useful to unsubscribe when necessary
+    this.resizeCheck = this._chartService.drawChartNew({
+      chartType: this.baD3ChartType,
+      element: this._selector.nativeElement,
+      inputData: this.baD3ChartData
+    });
+  }
 
-    ngOnDestroy():void {
-        this._chartService.removeChart(this._selector.nativeElement);
-        this.resizeCheck.unsubscribe();
-    }
+  ngOnDestroy() {
+    this._chartService.removeChart(this._selector.nativeElement);
+    this.resizeCheck.unsubscribe();
+  }
 
 }
