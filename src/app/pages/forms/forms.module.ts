c9534fd161dc0c32fba17152d412dc9139e22dc5
Xing <zhangxingW@hotmail.com>
2017-01-10 11:07:34 -0800

    chore(package): update ng2-bootstrap to 1.3.0

--- src/app/pages/forms/forms.module.ts
+++ src/app/pages/forms/forms.module.ts
@@ -5,7 +5,7 @@ import { NgaModule } from '../../theme/nga.module';
 
 import { routing }       from './forms.routing';
 
-import { RatingModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { RatingModule } from 'ng2-bootstrap';
 import { Forms } from './forms.component';
 import { Inputs } from './components/inputs';
 import { Layouts } from './components/layouts';
@@ -28,7 +28,7 @@ import { WithoutLabelsForm } from './components/layouts/components/withoutLabels
     CommonModule,
     AngularFormsModule,
     NgaModule,
-    RatingModule,
+    RatingModule.forRoot(),
     routing
   ],
   declarations: [
