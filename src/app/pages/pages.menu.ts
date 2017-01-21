be9c677c0606635521e7da674dc40c1a1b32228d
Alexander Zhukov <a.zhukov@akveo.com>
2017-01-21 13:10:17 +0300

    fix(dependencies): update angular to 2.4.4

--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -36,28 +36,28 @@ export const PAGES_MENU = [
           }
         ]
       },
-      //{
-      //  path: 'components',
-      //  data: {
-      //    menu: {
-      //      title: 'Components',
-      //      icon: 'ion-gear-a',
-      //      selected: false,
-      //      expanded: false,
-      //      order: 250,
-      //    }
-      //  },
-      //  children: [
-      //    {
-      //      path: 'treeview',
-      //      data: {
-      //        menu: {
-      //          title: 'Tree View',
-      //        }
-      //      }
-      //    }
-      //  ]
-      //},
+      // {
+      //   path: 'components',
+      //   data: {
+      //     menu: {
+      //       title: 'Components',
+      //       icon: 'ion-gear-a',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 250,
+      //     }
+      //   },
+      //   children: [
+      //     {
+      //       path: 'treeview',
+      //       data: {
+      //         menu: {
+      //           title: 'Tree View',
+      //         }
+      //       }
+      //     }
+      //   ]
+      // },
       {
         path: 'charts',
         data: {
@@ -184,14 +184,14 @@ export const PAGES_MENU = [
               }
             }
           },
-          {
-            path: 'smarttables',
-            data: {
-              menu: {
-                title: 'Smart Tables',
-              }
-            }
-          }
+          // {
+          //   path: 'smarttables',
+          //   data: {
+          //     menu: {
+          //       title: 'Smart Tables',
+          //     }
+          //   }
+          // }
         ]
       },
       {
