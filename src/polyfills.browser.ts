1cce1b5f753a5d6326b249ba6425411e150cc139
Alexander Zhukov <a.zhukov@akveo.com>
2017-02-14 16:34:28 +0300

    fix(dependencies): update angular to 2.4.7

--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -28,9 +28,6 @@ import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
-// Typescript emit helpers polyfill
-import 'ts-helpers';
-
 if ('production' === ENV) {
   // Production
 
