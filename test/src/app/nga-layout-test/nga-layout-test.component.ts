efbc8273e2f4c63edb19d4011852fd547b44102d
Alexander Zhukov <a.zhukov@akveo.com>
2017-02-20 17:22:05 +0300

    fix(components): fix border radius of color card, adjust layout styles

--- test/src/app/nga-layout-test/nga-layout-test.component.ts
+++ test/src/app/nga-layout-test/nga-layout-test.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nga-layout-test',
+  styleUrls: ['./nga-layout-test.component.scss'],
   template: `
     <nga-layout>
-      <nga-layout-sidebar>
+      <nga-layout-sidebar class="sidebar" left>
         <span>Sidebar</span>
       </nga-layout-sidebar>
-      <nga-layout-content>
+      <nga-layout-content class="content">
         <span>Content</span>
       </nga-layout-content>
     </nga-layout>
