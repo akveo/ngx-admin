7215b0b636b2d3ba23560bbeee6745a6677b17d6
Dmitry Nehaychik <4dmitr@gmail.com>
2017-02-21 11:52:13 +0300

    fix(build): hopefully fix the prod & aot build errors (#753)
    * fix(dependencies): fix versions
    
    * fix(build): get initial.scss back, updat some dependencies, fix resolve-url-loader warning
    
    * fix(build): fix out, update travis build
    
    * fix(build): revert sass-loader
    
    * fix(build): pin sass-loader at 4.1.1
    
    * fix(build): remove cache from travis

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -162,9 +162,9 @@ module.exports = function (options) {
 
         {
           test: /initial\.scss$/,
-          loader: ExtractTextPlugin.extract({
-            fallbackLoader: 'style-loader',
-            loader: 'css-loader!sass-loader?sourceMap'
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader!sass-loader?sourceMap'
           })
         },
 
