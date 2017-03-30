463954c81ca1338abed82100b5ca62bd08150e8b
Dmitry Nehaychik <4dmitr@gmail.com>
2017-03-30 16:59:57 +0300

    feat(menu): menu TODOs

--- src/framework/theme/components/menu/menu.interfaces.ts
+++ src/framework/theme/components/menu/menu.interfaces.ts
@@ -1,5 +1,7 @@
+// TODO: let's move this into the component (angular doesn't recommend using .interface files)
 export class NgaMenuItem {
   title: string;
+  // TODO let's add a 'link' property which accepts routerLink values
   url?: string;
   icon?: string;
   expanded?: boolean = false;
