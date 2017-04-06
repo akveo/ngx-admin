c9da9da9bf535d3942d34b7c61c12993484c9d15
Dmitry Nehaychik <4dmitr@gmail.com>
2017-04-06 20:58:49 +0300

    feat(auth): todos

--- src/framework/auth/index.ts
+++ src/framework/auth/index.ts
@@ -12,10 +12,13 @@ export * from './pages/request-password/request-password-page.component';
 export * from './pages/reset-password/reset-password-page.component';
 
 export * from './services/auth.service';
+export * from './services/token.service';
 export * from './providers/abstract-auth.provider';
 export * from './providers/dummy-auth.provider';
 export * from './providers/email-pass-auth.provider';
 
 
-// TODO: 1) how to reference the other modules (to NgaTheme, NgaCommon)?
 // TODO: 2) how to reference the theme CSS files?
+// TODO: 3) validation
+// TODO: 4) custom headers
+
