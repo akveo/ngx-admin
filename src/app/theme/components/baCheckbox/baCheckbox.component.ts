af108d113720cc95aaf0e93f98863445e62b3571
Mikhail Hryb <iamfreespirit.mg@gmail.com>
2017-04-06 19:08:49 +0300

    - fix (baMultiCheckbox) - fix baMultiCheckbox flex behaviour

--- src/app/theme/components/baCheckbox/baCheckbox.component.ts
+++ src/app/theme/components/baCheckbox/baCheckbox.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, Self} from '@angular/core';
-import {ControlValueAccessor, NgModel} from '@angular/forms';
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'ba-checkbox[ngModel]',
@@ -8,30 +8,37 @@ import {ControlValueAccessor, NgModel} from '@angular/forms';
   providers: [NgModel]
 })
 export class BaCheckbox implements ControlValueAccessor {
-  @Input() disabled:boolean;
-  @Input() label:string;
-  @Input() value:string;
-  @Input() baCheckboxClass:string;
+  @Input() disabled: boolean;
+  @Input() label: string;
+  @Input() value: string;
+  @Input() baCheckboxClass: string;
 
   public model: NgModel;
   public state: boolean;
 
-  public constructor(@Self() state:NgModel) {
+  public constructor(@Self() state: NgModel) {
     this.model = state;
     state.valueAccessor = this;
   }
 
-  public onChange(value: any): void {}
-  public onTouch(value: any): void {}
+  public onChange(value: any): void {
+  }
+
+  public onTouch(value: any): void {
+  }
+
   public writeValue(state: any): void {
     this.state = state;
   }
 
   public registerOnChange(fn: any): void {
-    this.onChange = function(state: boolean) {
+    this.onChange = function (state: boolean) {
       this.writeValue(state);
       this.model.viewToModelUpdate(state);
-    }
+    };
   }
-  public registerOnTouched(fn: any): void { this.onTouch = fn; }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
 }
