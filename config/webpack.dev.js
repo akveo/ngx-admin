d84e7ef76894b2274c75e819f7a502e469a5fe20
pluswave <zengbo.zhang@gmail.com>
2017-04-13 13:58:35 +0800

    build(webpack): fix a bug when PORT env exist. (#701)

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -16,7 +16,7 @@ const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
  */
 const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
 const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = webpackMerge(commonConfig({env: ENV}).metadata, {
   host: HOST,
