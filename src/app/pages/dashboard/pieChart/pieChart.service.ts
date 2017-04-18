156c7318eb57a0bf6b63352649d1ee14f06f4d12
Venkatraman <ramsunvtech@gmail.com>
2017-04-18 09:48:05 +0530

    Updated the Translation for Dashboard, Pages, Traffic chart, Popular App, Tree View and Chart JS

--- src/app/pages/dashboard/pieChart/pieChart.service.ts
+++ src/app/pages/dashboard/pieChart/pieChart.service.ts
@@ -12,22 +12,22 @@ export class PieChartService {
     return [
       {
         color: pieColor,
-        description: 'general.dashboard.newVisits',
+        description: 'dashboard.new_visits',
         stats: '57,820',
         icon: 'person',
       }, {
         color: pieColor,
-        description: 'general.dashboard.purchases',
+        description: 'dashboard.purchases',
         stats: '$ 89,745',
         icon: 'money',
       }, {
         color: pieColor,
-        description: 'general.dashboard.activeUsers',
+        description: 'dashboard.active_users',
         stats: '178,391',
         icon: 'face',
       }, {
         color: pieColor,
-        description: 'general.dashboard.returned',
+        description: 'dashboard.returned',
         stats: '32,592',
         icon: 'refresh',
       }
