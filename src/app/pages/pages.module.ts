b414aeb4964918264a7abeffea6be18adfb3f6a5
Venkatraman <ramsunvtech@gmail.com>
2017-04-18 05:52:58 -0500

    refactor: update the translation for Dashboard, Pages, Traffic chart, Popular App, Tree View and Chart JS (#984)

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,11 +3,12 @@ import { CommonModule }  from '@angular/common';
 
 import { routing }       from './pages.routing';
 import { NgaModule } from '../theme/nga.module';
+import { AppTranslationModule } from '../app.translation.module';
 
 import { Pages } from './pages.component';
 
 @NgModule({
-  imports: [CommonModule, NgaModule, routing],
+  imports: [CommonModule, AppTranslationModule, NgaModule, routing],
   declarations: [Pages]
 })
 export class PagesModule {
