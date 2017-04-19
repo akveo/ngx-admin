e033edf83871f0c896d69745b4c50da685ccaa07
Venkatraman <ramsunvtech@gmail.com>
2017-04-19 03:14:21 -0500

    feat(login): add the locale changes (#993)

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
+import { AppTranslationModule } from '../../app.translation.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgaModule } from '../../theme/nga.module';
 
@@ -10,6 +11,7 @@ import { routing }       from './login.routing';
 @NgModule({
   imports: [
     CommonModule,
+    AppTranslationModule,
     ReactiveFormsModule,
     FormsModule,
     NgaModule,
