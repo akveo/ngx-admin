9740eec18af2c3caf5632260fef8d3e01b98999d
Alexander Zhukov <a.zhukov@akveo.com>
2017-04-20 13:58:24 +0300

    fix(aio): remove the demo project, update angular to 4.0.2, remove ng-bootstrap

--- demo/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-export const environment = {
-  production: true,
-};
