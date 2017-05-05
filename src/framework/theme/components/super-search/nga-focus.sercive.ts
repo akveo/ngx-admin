0e8a2dd38d17d9418d3d5cfdb68bfb1029a2758a
mkolesnikov <mishkolesnikov@gmail.com>
2017-05-05 19:03:04 +0300

    feat(super-search): handle focus state

---
+++ src/framework/theme/components/super-search/nga-focus.sercive.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by Admin on 05.05.2017.
+ */
--- src/framework/theme/components/super-search/super-search.component.ts
+++ src/framework/theme/components/super-search/super-search.component.ts
@@ -1,6 +1,6 @@
 import {
   Component, ChangeDetectionStrategy, OnInit, Input, HostBinding,
-  ComponentRef, AfterViewInit, Output, EventEmitter, ChangeDetectorRef, ViewChild
+  ComponentRef, AfterViewInit, Output, EventEmitter, ChangeDetectorRef, ViewChild, ElementRef, Renderer
 } from '@angular/core';
 import { NgaSuperSearchService } from "./super-search.service";
 import { NgaThemeService } from "../../services/theme.service";
@@ -8,27 +8,27 @@ import { NgaThemeService } from "../../services/theme.service";
 
 @Component({
   selector: 'nga-search-field',
-  styleUrls: ['./super-search.component.modal-zoomin.scss',
-    './super-search.component.layout-rotate.scss',
-    './super-search.component.modal-move.scss',
-    './super-search.component.field-zoomin.scss',
-    './super-search-5.component.scss',
-    './super-search.component.curtain.scss',
-    './super-search.component.column-curtain.scss',
-    './super-search.component.modal-drop.scss',
-    './super-search.component.modal-half.scss',
+  styleUrls: ['styles/super-search.component.modal-zoomin.scss',
+    'styles/super-search.component.layout-rotate.scss',
+    'styles/super-search.component.modal-move.scss',
+    'styles/super-search.component.field-zoomin.scss',
+    'styles/super-search-5.component.scss',
+    'styles/super-search.component.curtain.scss',
+    'styles/super-search.component.column-curtain.scss',
+    'styles/super-search.component.modal-drop.scss',
+    'styles/super-search.component.modal-half.scss',
   ],
   template: `
 <div class="search__content">
-  <div class="search" (keyup)="($event.keyCode == '27' && showSearch) ? searchToggle() : null">
+  <div class="search" (keyup)="($event.keyCode == '27') ? onSearchClose() : null">
   
-    <button class="search__btn-close" (click)="searchToggle()">
+    <button class="search__btn-close" (click)="onSearchClose()">
       <i class="ion-close-round icon"></i>
     </button>
     <div class="form__wrapper">
-      <form class="search__form" (keyup.enter)="showSearch ? onSearchSubmit(searchInput.value) : null">
+      <form class="search__form" (keyup.enter)="onSearchSubmit(searchInput.value)">
         <div class="form__content">
-          <input #searchInput class="search__input" placeholder="akveo"/>
+          <input #searchInput class="search__input" placeholder="akveo" />
         </div>
         <span class="search__info">Hit enter to search or ESC to close</span>
       </form>
@@ -50,10 +50,13 @@ export class NgaSearchFieldComponent {
   static readonly TYPE_MODAL_HALF = 'modal-half';
 
   @HostBinding('class.show') public showSearch: boolean = false;
+
   public searchType: string = 'rotate-layout';
 
   @Output() searchClose = new EventEmitter();
   @Output() search = new EventEmitter();
+
+  @ViewChild('searchInput') inputElement: ElementRef;
 
   constructor() { }
 
@@ -101,14 +104,12 @@ export class NgaSearchFieldComponent {
     this.searchType = val;
   }
 
-  searchToggle() {
-    this.showSearch = !this.showSearch;
+  onSearchClose() {
     this.searchClose.emit(true);
   }
 
   onSearchSubmit(term) {
     if (term) {
-      this.showSearch = !this.showSearch;
       this.search.emit(term);
     }
   }
@@ -132,10 +133,10 @@ export class NgaSearchFieldComponent {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 @Component({
   selector: 'nga-search',
-  styleUrls: ['./super-search.component.scss'],
+  styleUrls: ['styles/super-search.component.scss'],
   template: `
     <div class="search-wrap" [class.show]="showSearch">
-      <button class="search__btn-open" (click)="searchToggle()">
+      <button class="search__btn-open" (click)="onOpenSearch()">
         <i class="ion-android-search icon"></i>
       </button>
     </div>
@@ -146,72 +147,36 @@ export class NgaSearchComponent {
   static readonly TYPE_MODAL_ZOOMIN = 'modal-zoomin';
   static readonly TYPE_ROTATE_LAYOUT = 'rotate-layout';
   static readonly TYPE_MODAL_MOVE = 'modal-move';
-  static readonly TYPE_FIELD_ZOOMIN = 'field-zoomin';
   static readonly TYPE_CURTAIN = 'curtain';
   static readonly TYPE_COLUMN_CURTAIN = 'column-curtain';
   static readonly TYPE_MODAL_DROP = 'modal-drop';
   static readonly TYPE_MODAL_HALF = 'modal-half';
 
   public showSearch: boolean = false;
-  public searchType: string = 'rotate-layout';
+  @Input() private type: string = 'rotate-layout';
   private searchFieldComponentRef: ComponentRef<any> = null;
 
   constructor(private searchService: NgaSuperSearchService,
-    private themeService: NgaThemeService) {
+              private themeService: NgaThemeService) {
   }
 
-  @HostBinding('class.modal-zoomin')
-  get modalZoomin() {
-    return this.searchType === NgaSearchFieldComponent.TYPE_MODAL_ZOOMIN;
-  }
-
-  @HostBinding('class.rotate-layout')
-  get rotateLayout() {
-    return this.searchType === NgaSearchFieldComponent.TYPE_ROTATE_LAYOUT;
-  }
-  @HostBinding('class.modal-move')
-  get modalMove() {
-    return this.searchType === NgaSearchFieldComponent.TYPE_MODAL_MOVE;
-  }
-
-  @HostBinding('class.field-zoomin')
-  get fieldZoomin() {
-    return this.searchType === NgaSearchFieldComponent.TYPE_FIELD_ZOOMIN;
-  }
-
-  @HostBinding('class.curtain')
-  get curtain() {
-    return this.searchType === NgaSearchFieldComponent.TYPE_CURTAIN;
-  }
-
-  @HostBinding('class.column-curtain')
-  get columnCurtain() {
-    return this.searchType === NgaSearchFieldComponent.TYPE_COLUMN_CURTAIN;
-  }
-
-  @HostBinding('class.modal-drop')
-  get modalDrop() {
-    return this.searchType === NgaSearchFieldComponent.TYPE_MODAL_DROP;
-  }
-
-  @HostBinding('class.modal-half')
-  get modalHalf() {
-    return this.searchType === NgaSearchFieldComponent.TYPE_MODAL_HALF;
-  }
-
-  @Input()
-  set type(val: string) {
-    this.searchType = val;
-  }
-
-  searchToggle() {
-    this.showSearch = !this.showSearch;
-
-    this.searchService.searchActivate(this.searchType, this.showSearch);
+  onOpenSearch() {
+    this.showSearch = true;
+    this.searchService.searchActivate(this.type, true);
     setTimeout(() => {
-      this.searchFieldComponentRef.instance.showSearch = this.showSearch;
+      this.searchFieldComponentRef.instance.showSearch = true;
+      this.searchFieldComponentRef.instance.inputElement.nativeElement.focus();
       this.searchFieldComponentRef.changeDetectorRef.detectChanges();
-    }, 1);
+    }, 1)
+  }
+
+  onCloseSearch() {
+    this.showSearch = false;
+    this.searchService.searchActivate(this.type, false);
+    this.searchFieldComponentRef.instance.showSearch = false;
+    this.searchFieldComponentRef.instance.inputElement.nativeElement.value = '';
+    this.searchFieldComponentRef.instance.inputElement.nativeElement.blur();
+    this.searchFieldComponentRef.changeDetectorRef.detectChanges();
   }
 
   ngAfterViewInit() {
@@ -226,35 +191,16 @@ export class NgaSearchComponent {
   }
 
   connectToSearchField(component) {
-    component.searchType = this.searchType;
+    component.searchType = this.type;
 
     component.searchClose.subscribe(() => {
-      this.showSearch = !this.showSearch;
-      this.searchService.searchActivate(this.searchType, this.showSearch);//TODO: try to use theme-service
+      this.onCloseSearch();
     })
 
     component.search.subscribe(term => {
-      this.showSearch = !this.showSearch;
       this.searchService.search(term);
-      this.searchService.searchActivate(this.searchType, this.showSearch);//TODO: try to use theme-service
+      this.onCloseSearch();
     })
   }
-
-  // connectToRemoteSearchField(componentRef) {
-  //   this.searchFieldComponentRef = componentRef;
-  //   componentRef.instance.searchType = this.searchType;
-  //   this.searchFieldComponentRef.changeDetectorRef.detectChanges();
-  //
-  //   componentRef.instance.searchClose.subscribe(() => {
-  //     this.showSearch = !this.showSearch;
-  //     this.searchService.searchActivate(this.searchType, this.showSearch);
-  //   })
-  //
-  //   componentRef.instance.search.subscribe(term => {
-  //     this.showSearch = !this.showSearch;
-  //     this.searchService.search(term);
-  //     this.searchService.searchActivate(this.searchType, this.showSearch);
-  //   })
-  // }
 }
 
