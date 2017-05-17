6608298924d8e124b308147ec011cbbf1ccddc3f
Alexander Zhukov <a.zhukov@akveo.com>
2017-05-17 15:01:47 +0300

    refactor(theme): refactor a charts module

--- src/framework/theme/overrides/charts/charts.module.ts
+++ src/framework/theme/overrides/charts/charts.module.ts
@@ -8,16 +8,21 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularEchartsModule } from 'angular2-echarts';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
+const modules = [
+  AngularEchartsModule,
+  NgxChartsModule,
+  ChartsModule,
+];
 
 @NgModule({
   imports: [
     CommonModule,
-    AngularEchartsModule,
-    NgxChartsModule,
+    ...modules,
   ],
   exports: [
-    AngularEchartsModule,
-    NgxChartsModule,
+    ...modules,
   ],
 })
 export class NgaChartsModule {
@@ -25,12 +30,10 @@ export class NgaChartsModule {
     return <ModuleWithProviders>{
       ngModule: NgaChartsModule,
       imports: [
-        AngularEchartsModule,
-        NgxChartsModule,
+        ...modules,
       ],
       exports: [
-        AngularEchartsModule,
-        NgxChartsModule,
+        ...modules,
       ],
     };
   }
