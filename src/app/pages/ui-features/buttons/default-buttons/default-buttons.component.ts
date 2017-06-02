774fe48d65a2be078e1563db6aee1423861f8efc
Alexander Zhukov <a.zhukov@akveo.com>
2017-06-02 16:05:11 +0300

    refactor(buttons): fix styles

--- src/app/pages/ui-features/buttons/default-buttons/default-buttons.component.ts
+++ src/app/pages/ui-features/buttons/default-buttons/default-buttons.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-default-buttons',
-  styleUrls: ['./default-buttons.component.scss'],
   templateUrl: './default-buttons.component.html',
 })
 export class DefaultButtonsComponent {
