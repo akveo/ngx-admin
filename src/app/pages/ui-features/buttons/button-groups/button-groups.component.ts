224e0d339025f36d9dc3afdd1b8ddb08385667ba
Alexander Zhukov <a.zhukov@akveo.com>
2017-06-26 15:33:19 +0300

    feat(buttons): add new examples

--- src/app/pages/ui-features/buttons/button-groups/button-groups.component.ts
+++ src/app/pages/ui-features/buttons/button-groups/button-groups.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './button-groups.component.html',
 })
 export class ButtonGroupsComponent {
+
+  radioModel = 'Left';
+
+  checkboxModel = {
+    left: true,
+    middle: false,
+    right: true,
+  };
+
+  dividedCheckboxModel = {
+    monday: true,
+    tuesday: true,
+    wednesday: false,
+    thursday: false,
+    friday: false,
+    saturday: false,
+  };
 }
