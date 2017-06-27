b9a246457fa08763f3c327f74869b43635c94146
KostyaDanovsky <kostya.danovsky@gmail.com>
2017-06-27 12:28:07 +0300

    fix(buttons): move styles to related child components

--- src/app/pages/ui-features/buttons/default-buttons/default-buttons.component.ts
+++ src/app/pages/ui-features/buttons/default-buttons/default-buttons.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-default-buttons',
+  styleUrls: ['./default-buttons.component.scss'],
   templateUrl: './default-buttons.component.html',
 })
 export class DefaultButtonsComponent {
