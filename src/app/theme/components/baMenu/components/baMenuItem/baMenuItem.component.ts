6575f1e04322aaf71e31800930ed5f35f4f48c83
Newan <jens.nachtigal@rz-products.com>
2017-06-28 10:32:22 +0200

    fix(baMenuItem): Hidden Children doen´t show

--- src/app/theme/components/baMenu/components/baMenuItem/baMenuItem.component.ts
+++ src/app/theme/components/baMenu/components/baMenuItem/baMenuItem.component.ts
@@ -1,25 +1,36 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'ba-menu-item',
   templateUrl: './baMenuItem.html',
-  styleUrls: ['./baMenuItem.scss']
+  styleUrls: ['./baMenuItem.scss'],
 })
 export class BaMenuItem {
 
-  @Input() menuItem:any;
-  @Input() child:boolean = false;
+  @Input() menuItem: any;
+  @Input() child: boolean = false;
 
   @Output() itemHover = new EventEmitter<any>();
   @Output() toggleSubMenu = new EventEmitter<any>();
 
-  public onHoverItem($event):void {
+  public onHoverItem($event): void {
     this.itemHover.emit($event);
   }
 
-  public onToggleSubMenu($event, item):boolean {
+  public onToggleSubMenu($event, item): boolean {
     $event.item = item;
     this.toggleSubMenu.emit($event);
+    return false;
+  }
+
+  private checkChildrenView(): boolean {
+    if (this.menuItem.children) {
+      for (let childrenItem of this.menuItem.children) {
+        if (!childrenItem.hidden) {
+          return true;
+        }
+      }
+    }
     return false;
   }
 }
