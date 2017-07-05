d8b7c81f46177dd82e95637cab989148d4ad35f1
KostyaDanovsky <kostya.danovsky@gmail.com>
2017-07-05 20:18:53 +0300

    feat(dashboard): add recent tab and add scroll to contacts

--- src/app/pages/dashboard/contacts/contacts.component.ts
+++ src/app/pages/dashboard/contacts/contacts.component.ts
@@ -8,7 +8,10 @@ import { UserService } from '../../../@core/data/users.service';
   templateUrl: './contacts.component.html',
 })
 export class ContactsComponent {
+
   contacts: any[];
+
+  recent: any[];
 
   constructor(private userService: UserService) {
     const users = this.userService.getUsers();
@@ -20,6 +23,17 @@ export class ContactsComponent {
       { user: users.lee, type: 'mobile' },
       { user: users.alan, type: 'home' },
       { user: users.kate, type: 'work' },
+    ];
+
+    this.recent = [
+      { user: users.alan, type: 'home', time: '9:12 pm' },
+      { user: users.eva, type: 'home', time: '7:45 pm' },
+      { user: users.nick, type: 'mobile', time: '5:29 pm' },
+      { user: users.lee, type: 'mobile', time: '11:24 am' },
+      { user: users.jack, type: 'mobile', time: '10:45 am' },
+      { user: users.kate, type: 'work', time: '9:42 am' },
+      { user: users.kate, type: 'work', time: '9:31 am' },
+      { user: users.jack, type: 'mobile', time: '8:01 am' },
     ];
   }
 }
