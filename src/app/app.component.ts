7afa4ffb8901adff9e9e36d1aa69c4f8c0150050
JonasHess <JonasHess@users.noreply.github.com>
2017-07-08 00:48:56 +0200

    Removed absolute image path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,7 +48,7 @@ export class App {
 
   private _loadImages(): void {
     // register some loaders
-    BaThemePreloader.registerLoader(this._imageLoader.load('/assets/img/sky-bg.jpg'));
+    BaThemePreloader.registerLoader(this._imageLoader.load('assets/img/sky-bg.jpg'));
   }
 
 }
