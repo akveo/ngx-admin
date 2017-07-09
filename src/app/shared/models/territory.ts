38f37c47c36d38db0f8b68e43d9d95ecd03c3ab9
Valter Gomes <valter.junior@soaexpert.com.br>
2017-07-09 08:04:02 -0300

    Create a Territory Model

---
+++ src/app/shared/models/territory.ts
@@ -0,0 +1,14 @@
+
+
+export enum TerritoryTypeEnum {
+        CONFIRMED = <any>"territoriesConfirmed" ,
+        TBC = <any>"territoriesTbc"    
+        }
+
+export class Territory{
+ 
+    constructor(){
+
+    }
+
+     }
