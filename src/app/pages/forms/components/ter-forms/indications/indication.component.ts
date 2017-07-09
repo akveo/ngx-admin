a9d3094a23330860254cb221cdfdb5bbf20064f2
Valter Gomes <valter.junior@soaexpert.com.br>
2017-07-09 07:58:29 -0300

    Notification to feedback the user
    A NG4 lib to feedback user and improve it's experience

--- src/app/pages/forms/components/ter-forms/indications/indication.component.ts
+++ src/app/pages/forms/components/ter-forms/indications/indication.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {IndicationService} from './indication.service';
+import { NotificationsService } from "angular2-notifications";
 
 @Component({
   selector: 'indication',
@@ -8,9 +9,27 @@ import {IndicationService} from './indication.service';
 export class IndicationComponent{
 
   constructor(private indicationService:IndicationService) {
+  constructor(private territoryService:TerritoriesService , private _service: NotificationsService) {
   }
 
 addNewIndication(event){
   this.indicationService.addNewIndication(event.value);
 }
+  public options = {
+      position: ["bottom", "right"],
+      timeOut: 50000,
+      lastOnBottom: true,
+  };
+  notify() {
+    this._service.success(
+        'Territory Successfuly Created',
+        'Some Content',
+        {
+            showProgressBar: true,
+            pauseOnHover: true,
+            clickToClose: true,
+            maxLength: 10
+        }
+    )
+  } 
 }
