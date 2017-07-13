009e5788acc28026d5e658b558db2d6fa373fd27
Dmitry Nehaychik <4dmitr@gmail.com>
2017-07-13 11:36:59 +0300

    feat(typography): first bunch of typography

--- src/app/pages/ui-features/typography/typography.component.ts
+++ src/app/pages/ui-features/typography/typography.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-typography',
+  styleUrls: ['./typography.component.scss'],
   templateUrl: './typography.component.html',
 })
 export class TypographyComponent {
