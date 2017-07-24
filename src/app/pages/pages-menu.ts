52f49856b197ae9480c09288c358a3622109b998
Alexander Zhukov <a.zhukov@akveo.com>
2017-07-24 20:14:06 +0300

    style(menu): fix array style

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,134 +2,98 @@ import { NgaMenuItem } from '@akveo/nga-theme';
 
 import { List } from 'immutable';
 
-export const MENU_ITEMS: List<NgaMenuItem> = List([
-  {
-    title: 'Dashboard',
-    icon: 'ion ion-ios-home-outline',
-    link: '/pages/dashboard',
-    home: true,
-  },
-  {
-    title: 'FEATURES',
-    group: true,
-  },
-  {
-    title: 'UI Features',
-    icon: 'ion ion-ios-keypad-outline',
-    link: '/pages/ui-features',
-    children: List<NgaMenuItem>([
-      {
-        title: 'Buttons',
-        link: '/pages/ui-features/buttons',
-      },
-      {
-        title: 'Grid',
-        link: '/pages/ui-features/grid',
-      },
-      {
-        title: 'Icons',
-        link: '/pages/ui-features/icons',
-      },
-      {
-        title: 'Modals',
-        link: '/pages/ui-features/modals',
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-      {
-        title: 'Tabs',
-        link: '/pages/ui-features/tabs',
-      },
-    ]),
-  },
-  {
-    title: 'Components',
-    icon: 'ion ion-ios-gear-outline',
-    children: List<NgaMenuItem>([
-      {
-        title: 'Tree',
-        link: '/pages/components/tree',
-      }, {
-        title: 'Notifications',
-        link: '/pages/components/notifications',
-      },
-    ]),
-  },
-  {
-    title: 'Maps',
-    icon: 'ion ion-ios-location-outline',
-    children: List<NgaMenuItem>([
-      {
-        title: 'Gmaps',
-        link: '/pages/maps/gmaps',
-      },
-      {
-        title: 'Leaflet',
-        link: '/pages/maps/leaflet',
-      },
-    ]),
-  },
-  {
-    title: 'Charts',
-    icon: 'ion ion-arrow-graph-up-right',
-    children: List<NgaMenuItem>([
-      {
-        title: 'Echarts',
-        link: '/pages/charts/echarts',
-      },
-      {
-        title: 'D3',
-        link: '/pages/charts/d3',
-      },
-      {
-        title: 'Charts.js',
-        link: '/pages/charts/chartjs',
-      },
-    ]),
-  },
-  {
-    title: 'Editors',
-    icon: 'ion ion-edit',
-    children: List<NgaMenuItem>([
-      {
-        title: 'TinyMCE',
-        link: '/pages/editors/tinymce',
-      },
-      {
-        title: 'CKEditor',
-        link: '/pages/editors/ckeditor',
-      },
-    ]),
-  },
-  {
-    title: 'Forms',
-    icon: 'ion-compose',
-    children: List<NgaMenuItem>([
-      {
-        title: 'Form Inputs',
-        link: '/pages/forms/inputs',
-      },
-
-      {
-        title: 'Form Layouts',
-        link: '/pages/forms/layouts',
-      },
-    ]),
-  },
-  {
-    title: 'Tables',
-    icon: 'ion-ios-grid-view',
-    children: List<NgaMenuItem>([
-      {
-        title: 'Smart Table',
-        link: '/pages/tables/smart-table',
-      },
-    ]),
-  },
-]);
+export const MENU_ITEMS: List<NgaMenuItem> = List([{
+  title: 'Dashboard',
+  icon: 'ion ion-ios-home-outline',
+  link: '/pages/dashboard',
+  home: true,
+}, {
+  title: 'FEATURES',
+  group: true,
+}, {
+  title: 'UI Features',
+  icon: 'ion ion-ios-keypad-outline',
+  link: '/pages/ui-features',
+  children: List<NgaMenuItem>([{
+    title: 'Buttons',
+    link: '/pages/ui-features/buttons',
+  }, {
+    title: 'Grid',
+    link: '/pages/ui-features/grid',
+  }, {
+    title: 'Icons',
+    link: '/pages/ui-features/icons',
+  }, {
+    title: 'Modals',
+    link: '/pages/ui-features/modals',
+  }, {
+    title: 'Typography',
+    link: '/pages/ui-features/typography',
+  }, {
+    title: 'Animated Searches',
+    link: '/pages/ui-features/search-fields',
+  }, {
+    title: 'Tabs',
+    link: '/pages/ui-features/tabs',
+  }]),
+}, {
+  title: 'Forms',
+  icon: 'ion-compose',
+  children: List<NgaMenuItem>([{
+    title: 'Form Inputs',
+    link: '/pages/forms/inputs',
+  }, {
+    title: 'Form Layouts',
+    link: '/pages/forms/layouts',
+  }]),
+}, {
+  title: 'Components',
+  icon: 'ion ion-ios-gear-outline',
+  children: List<NgaMenuItem>([{
+    title: 'Tree',
+    link: '/pages/components/tree',
+  }, {
+    title: 'Notifications',
+    link: '/pages/components/notifications',
+  }]),
+}, {
+  title: 'Maps',
+  icon: 'ion ion-ios-location-outline',
+  children: List<NgaMenuItem>([{
+    title: 'Gmaps',
+    link: '/pages/maps/gmaps',
+  }, {
+    title: 'Leaflet',
+    link: '/pages/maps/leaflet',
+  }]),
+}, {
+  title: 'Charts',
+  icon: 'ion ion-arrow-graph-up-right',
+  children: List<NgaMenuItem>([{
+    title: 'Echarts',
+    link: '/pages/charts/echarts',
+  }, {
+    title: 'D3',
+    link: '/pages/charts/d3',
+  }, {
+    title: 'Charts.js',
+    link: '/pages/charts/chartjs',
+  }]),
+}, {
+  title: 'Editors',
+  icon: 'ion ion-edit',
+  children: List<NgaMenuItem>([{
+    title: 'TinyMCE',
+    link: '/pages/editors/tinymce',
+  }, {
+    title: 'CKEditor',
+    link: '/pages/editors/ckeditor',
+  }]),
+}, {
+  title: 'Tables',
+  icon: 'ion-ios-grid-view',
+  children: List<NgaMenuItem>([{
+    title: 'Smart Table',
+    link: '/pages/tables/smart-table',
+  }]),
+}]);
