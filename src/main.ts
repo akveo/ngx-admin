acf1351b02976eb4a7493f38e8bba65b61d74100
Hrvoje Kusulja <hkusulja@kusulja.com>
2017-07-24 18:42:47 +0200

    Update main.ts
    Fix to import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { environment } from '././environments/environment';
 import { AppModule } from './app/app.module';
 
-if (environment.production==true) {
+if (environment.production) {
   enableProdMode();
 }
 
