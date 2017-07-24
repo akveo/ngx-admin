ed644c801b39d95c9f7c0c7341ba3abdf658e20b
Hrvoje Kusulja <hkusulja@kusulja.com>
2017-07-24 09:04:30 +0200

    Update main.ts for enableProdMode();
    Support for Angular Prod Mode when in production==true in environment.

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if (environment.production==true) {
+  enableProdMode();
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule);
