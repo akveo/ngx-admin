b58b8f934571ee291ece0f215f2f31299aba683e
Alexander Zhukov <a.zhukov@akveo.com>
2017-07-25 14:07:11 +0300

    fix(form): update checkbox and radio styles

--- src/app/pages/forms/form-layouts/form-layouts.component.ts
+++ src/app/pages/forms/form-layouts/form-layouts.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-form-layouts',
+  styleUrls: ['./form-layouts.component.scss'],
   templateUrl: './form-layouts.component.html',
 })
 export class FormLayoutsComponent {
