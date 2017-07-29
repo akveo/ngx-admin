7dc76be1077fd6e6912ea6cb8f9cee39513fb9b4
Dmitry Nehaychik <4dmitr@gmail.com>
2017-07-29 16:14:10 +0300

    feat(layout): add withScroll setting

--- src/framework/theme/components/layout/layout.component.ts
+++ src/framework/theme/components/layout/layout.component.ts
@@ -204,6 +204,7 @@ export class NgaLayoutComponent implements OnDestroy, AfterViewInit {
 
   centerValue: boolean = false;
   @HostBinding('class.window-mode') windowModeValue: boolean = false;
+  @HostBinding('class.with-scroll') withScrollValue: boolean = false;
 
   /**
    * Defines whether the layout columns will be centered after some width
@@ -217,11 +218,31 @@ export class NgaLayoutComponent implements OnDestroy, AfterViewInit {
   /**
    * Defines whether the layout enters a 'window' mode, when the layout content (including sidebars and fixed header)
    * becomes centered by width with a margin from the top of the screen, like a floating window.
+   * Automatically enables `withScroll` mode, as in the window mode scroll must be inside the layout and cannot be on
+   * window. (TODO: check this)
    * @param {boolean} val
    */
   @Input()
   set windowMode(val: boolean) {
     this.windowModeValue = convertToBoolProperty(val);
+    this.withScroll = true;
+  }
+
+  /**
+   * Defines whether to move the scrollbars to layout or leave it at the body level.
+   * Automatically set to true when `windowMode` is enabled.
+   * @param {boolean} val
+   */
+  @Input()
+  set withScroll(val: boolean) {
+    this.withScrollValue = convertToBoolProperty(val);
+
+    // TODO: is this the best way of doing it? as we don't have access to body from theme styles
+    if (this.withScrollValue) {
+      document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+    } else {
+      document.getElementsByTagName('body')[0].style.overflow = 'initial';
+    }
   }
 
   @ViewChild('layoutTopDynamicArea', { read: ViewContainerRef }) veryTopRef: ViewContainerRef;
