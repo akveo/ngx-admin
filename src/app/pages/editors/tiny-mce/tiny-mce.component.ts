9d1697776e4b13275262a7f1d09c4c6d2f2e53f9
KostyaDanovsky <kostya.danovsky@gmail.com>
2017-09-14 16:18:46 +0300

    fix(dashboard): improve temperature and tiny-mce responsiveness

--- src/app/pages/editors/tiny-mce/tiny-mce.component.ts
+++ src/app/pages/editors/tiny-mce/tiny-mce.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-tiny-mce-page',
   template: `
-    <nb-card size="large">
+    <nb-card>
       <nb-card-header>
         Tiny MCE
       </nb-card-header>
