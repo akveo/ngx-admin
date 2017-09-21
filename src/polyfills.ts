e853c0783cd88f5f2e93be9b0f581ab67424c8ce
Dmitry Nehaychik <4dmitr@gmail.com>
2017-09-21 13:18:45 +0300

    fix(d3): fix d3 not being shown

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -69,7 +69,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 import 'intl';  // Run `npm install --save intl`.
-
+import 'intl/locale-data/jsonp/en';
 import 'core-js/es7/array';
 
 if (typeof SVGElement.prototype.contains === 'undefined') {
