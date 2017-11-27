446279e850e081483b59d67ae8903bf0e082b4cc
Fabián León <fadarsaleeing@gmail.com>
2017-11-26 23:45:50 -0500

    relizando pruebas de modulos, revisando angular-cli, construyendo modulo about, inspeccionando inputs.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,7 @@ import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ThemeModule } from '../@theme/theme.module';
+import { AboutComponent } from './about/about.component';
 
 const PAGES_COMPONENTS = [
   PagesComponent,
@@ -17,6 +18,7 @@ const PAGES_COMPONENTS = [
   ],
   declarations: [
     ...PAGES_COMPONENTS,
+    AboutComponent,
   ],
 })
 export class PagesModule {
