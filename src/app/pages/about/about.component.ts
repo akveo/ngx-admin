f87bff33bc0bc3bc96b0aea5ad3ade50524ce1ef
Fabio Parra <faparraf@gmail.com>
2017-11-27 10:04:20 -0500

    Update about.component.ts

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AboutComponent implements OnInit {
-  objeto : any;
+  objeto : any ; 
   constructor() {
     this.objeto = {};
   }
