9381d8c81e20933b18073d9af40471a7e2e7a14d
Dmitry Nehaychik <4dmitr@gmail.com>
2018-02-23 15:09:19 +0300

    feat(auth): configurable auth social icons (#1545)
    Closes #1515

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -8,6 +8,24 @@ import { throwIfAlreadyLoaded } from './module-import-guard';
 import { DataModule } from './data/data.module';
 import { AnalyticsService } from './utils/analytics.service';
 
+const socialLinks = [
+  {
+    url: 'https://github.com/akveo/nebular',
+    target: '_blank',
+    icon: 'socicon-github',
+  },
+  {
+    url: 'https://www.facebook.com/akveo/',
+    target: '_blank',
+    icon: 'socicon-facebook',
+  },
+  {
+    url: 'https://twitter.com/akveo_inc',
+    target: '_blank',
+    icon: 'socicon-twitter',
+  },
+];
+
 const NB_CORE_PROVIDERS = [
   ...DataModule.forRoot().providers,
   ...NbAuthModule.forRoot({
@@ -20,6 +38,14 @@ const NB_CORE_PROVIDERS = [
             rememberMe: true,
           },
         },
+      },
+    },
+    forms: {
+      login: {
+        socialLinks: socialLinks,
+      },
+      register: {
+        socialLinks: socialLinks,
       },
     },
   }).providers,
