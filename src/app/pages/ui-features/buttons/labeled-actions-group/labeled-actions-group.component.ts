ff6a915b773a05101dfd923ceb357c6babce1f5b
Dmitry Nehaychik <4dmitr@gmail.com>
2018-02-27 13:50:49 +0300

    build: starter-kit clean up

--- src/app/pages/ui-features/buttons/labeled-actions-group/labeled-actions-group.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-labeled-actions-group',
-  styleUrls: ['./labeled-actions-group.component.scss'],
-  templateUrl: './labeled-actions-group.component.html',
-})
-export class LabeledActionsGroupComponent {
-}
