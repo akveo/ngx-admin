34c319b11484382357f206648e2615d1cdccf330
Dmitry Nehaychik <4dmitr@gmail.com>
2018-03-01 14:15:58 +0300

    feat(demo): a/b testing for theme

--- src/app/@core/utils/analytics.service.ts
+++ src/app/@core/utils/analytics.service.ts
@@ -3,6 +3,7 @@ import { NavigationEnd, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { filter } from 'rxjs/operator/filter';
+import { NbThemeService } from '@nebular/theme';
 
 declare const ga: any;
 
@@ -10,8 +11,12 @@ declare const ga: any;
 export class AnalyticsService {
   private enabled: boolean;
 
-  constructor(private location: Location, private router: Router) {
+  constructor(private location: Location, private router: Router, private themeService: NbThemeService) {
     this.enabled = false;
+
+    document.addEventListener('variant-load', (e) => {
+      this.themeService.changeTheme('default');
+    }, false);
   }
 
   trackPageViews() {
