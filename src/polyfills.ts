373fe23ee545c55041ffa869534dc15e73ea1d27
Dmitry Nehaychik <4dmitr@gmail.com>
2018-03-02 14:17:14 +0300

    fix(ie): add es7 object polyfill for IE

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -71,6 +71,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 import 'intl';  // Run `npm install --save intl`.
 import 'intl/locale-data/jsonp/en';
 import 'core-js/es7/array';
+import 'core-js/es7/object';
 
 if (typeof SVGElement.prototype.contains === 'undefined') {
   SVGElement.prototype.contains = HTMLDivElement.prototype.contains;
