fb424378308ab3ae72275c107a1b6798d0c3b876
dimaatkaev <dimaatkaev@gmail.com>
2018-03-02 12:08:47 +0300

    feat(search-map): set default location Minsk (#1573)

--- src/app/pages/maps/search-map/entity/Location.ts
+++ src/app/pages/maps/search-map/entity/Location.ts
@@ -1,4 +1,4 @@
 export class Location {
-  constructor(public latitude: number = 54, public longitude: number = 33) {
+  constructor(public latitude: number = 53.9, public longitude: number = 27.5667) {
   }
 }
