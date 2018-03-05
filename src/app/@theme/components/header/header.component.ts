41d6293624cb96318ea2bcc7fab228ecc166b926
Fabian León <fadarsaleeing@gmail.com>
2018-03-05 17:33:32 -0500

    corrigiendo errores de build

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -25,7 +25,7 @@ export class HeaderComponent implements OnInit {
     private userService: UserService,
     private analyticsService: AnalyticsService,
     private autenticacion: AutenticationService,
-    private translate: TranslateService) {
+    public translate: TranslateService) {
     this.translate = translate;
   }
   useLanguage(language: string) {
