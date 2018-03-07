aaec4aafecc17948ec5744cbd2e4da104fe9948c
geiner <geiner.salcedo.ppc0111@gmail.com>
2018-03-07 09:47:25 -0500

    organizacion de codigo y despliegue en forma de componente como ejemplo

--- src/app/pages/inscripciones/info-basica/info-basica.component.ts
+++ src/app/pages/inscripciones/info-basica/info-basica.component.ts
@@ -4,54 +4,61 @@ import { PersonaService } from './../../../@core/data/persona.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ngx-info-basica',
+  selector: 'ngx-info-basica-persona',
   templateUrl: './info-basica.component.html',
   styleUrls: ['./info-basica.component.scss'],
 })
 export class InfoBasicaComponent implements OnInit {
 
   public formulario: any;
-  public ejemplo: any;
-  public percentage: any;
   public usuario: any;
 
   constructor(private persona: PersonaService, private autenticacion: AutenticationService) {
     this.formulario = FORM_PERSONA;
-    this.percentage = 0.0;
   }
 
-  ngOnInit() {
-    this.persona.get('persona/full/?userid=' + this.autenticacion.getPayload().sub)
+  cargarInfoPersona():void {
+    this.persona.get('persona/?query=Usuario:' + this.autenticacion.getPayload().sub)
         .subscribe(res => {
           if (res !== null) {
-            this.usuario = res;
-            this.usuario = this.usuario.Persona;
+            this.usuario = res[0];
             this.usuario.CiudadNacimiento = { Id : this.usuario.CiudadNacimiento };
-            console.info( this.usuario );
           }
       });
   }
 
-  traerDatosBasicos(event) {
-    if (event.valid) {
-      event.data.Persona.Usuario = this.autenticacion.getPayload().sub;
-      if (this.usuario === undefined) {
-        this.persona.post('persona', event.data.Persona)
+  actualizarInfoPersona(persona:any):void {
+    persona.Id=this.usuario.Id;
+    persona.usuario=this.usuario.Usuario;
+    this.persona.put('persona',persona)
+    .subscribe(res => {
+      this.cargarInfoPersona();
+    });
+  }
+
+  registrarPersona(persona:any):void {
+    persona.Usuario = this.autenticacion.getPayload().sub;
+    this.persona.post('persona', persona)
         .subscribe(res => {
         this.usuario = res;
+        //this.cargarInfoPersona();
         });
-      }else {
-        event.data.Persona.Id = this.usuario.Id;
-        this.persona.put('persona', event.data.Persona)
-        .subscribe(res => {
-          this.usuario = res;
-          console.info(res);
-        });
-      }
-
-    }
-
   }
+
+  ngOnInit() {
+    this.cargarInfoPersona();
+  }
+
+  validarForm(event) {
+    if (event.valid) {
+      if (this.usuario === undefined) {
+        this.registrarPersona(event.data.Persona);
+      }else {
+        this.actualizarInfoPersona(event.data.Persona);
+      }
+    }
+  }
+
 }
 
 
