e0e57545e512a8ed7aee314e071798406da642ac
Eugen Sadovsky <e.sadovsky@akveo.com>
2018-03-13 23:09:47 +0300

    feat(dashboard): highlight and click on map

--- src/app/pages/ec-dashboard/map/ec-map.component.ts
+++ src/app/pages/ec-dashboard/map/ec-map.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 import * as L from 'leaflet';
 import 'style-loader!leaflet/dist/leaflet.css';
 
-import { EcMapService } from './ec-map.service';
-import { Layer } from 'leaflet';
+import {EcMapService} from './ec-map.service';
 
 @Component({
   selector: 'ngx-ec-map',
@@ -20,23 +19,61 @@ import { Layer } from 'leaflet';
 })
 export class EcMapComponent {
 
-  layers: Layer[] = [];
-
-  constructor(private ecMapService: EcMapService) {
-
-  this.ecMapService.getCords()
-    .subscribe(cords => {
-      this.layers.push(L.geoJSON(
-        (cords) as any,
-        {style: () => ({color: '#00d977'})}));
-    });
-  }
+  layers = [];
 
   options = {
     layers: [
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '...'}),
     ],
     zoom: 5,
-    center: L.latLng({ lat: 38.991709, lng: -76.886109 }),
+    center: L.latLng({lat: 38.991709, lng: -76.886109}),
   };
+
+  constructor(private ecMapService: EcMapService) {
+    this.ecMapService.getCords()
+      .subscribe(cords => {
+        this.layers.push(this.createGeoJsonLayer(cords));
+      });
+  }
+
+  private createGeoJsonLayer(cords) {
+    return L.geoJSON(
+      (cords) as any,
+      {
+        style: () => ({color: '#00d977'}),
+        onEachFeature: (f, l) => { this.onEachFeature(f, l) },
+      })
+  }
+
+  private onEachFeature(feature, layer) {
+    layer.on({
+      mouseover: this.highlightFeature,
+      mouseout: (e) => { this.resetHighlight(e) },
+      click: this.selectFeature,
+    });
+  }
+
+  private highlightFeature(e) {
+    const layer = e.target;
+
+    layer.setStyle({
+      weight: 5,
+      color: '#3d6636',
+      dashArray: '',
+      fillOpacity: 0.7,
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera12 && !L.Browser.edge) {
+      layer.bringToFront();
+    }
+  }
+
+  private resetHighlight(e) {
+    const layer = this.layers[0];
+    layer.resetStyle(e.target);
+  }
+
+  private selectFeature(e) {
+  }
+
 }
