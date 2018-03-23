a517c70618913f93ce675526d50b1c5ae83d2137
Dmitry Nehaychik <4dmitr@gmail.com>
2018-03-23 17:20:43 +0300

    feat(demo): additional event tracking

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -1,27 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserService } from '../../../@core/data/users.service';
 import { AnalyticsService } from '../../../@core/utils/analytics.service';
+import { takeWhile } from 'rxjs/operators/takeWhile';
+import { fromEvent as observableFromEvent } from 'rxjs/observable/fromEvent';
 
 @Component({
   selector: 'ngx-header',
   styleUrls: ['./header.component.scss'],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit, OnDestroy {
 
   @Input() position = 'normal';
 
   user: any;
-
   userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
+
+  private alive = true;
 
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
               private userService: UserService,
+              private analytics: AnalyticsService,
               private analyticsService: AnalyticsService) {
+
+    observableFromEvent(document, 'mouseup')
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(() => {
+        let selection: any;
+        if (window.getSelection) {
+          selection = window.getSelection();
+        } else if ((<any> document).selection) {
+          selection = (<any> document).selection.createRange();
+        }
+
+        if (selection && selection.toString() === 'contact@akveo.com') {
+          this.analytics.trackEvent('contactEmail', 'select');
+        }
+      });
   }
 
   ngOnInit() {
@@ -45,5 +63,13 @@ export class HeaderComponent implements OnInit {
 
   startSearch() {
     this.analyticsService.trackEvent('startSearch');
+  }
+
+  trackEmailClick() {
+    this.analytics.trackEvent('contactEmail', 'click');
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
   }
 }
