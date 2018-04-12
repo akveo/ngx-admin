f67c17a463b6c5f1520582b217f0c8f5e4c010c1
Fabián León <fadarsaleeing@gmail.com>
2018-04-12 01:51:41 -0500

    lint

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -14,49 +14,49 @@ export class FooterComponent {
       subtitle: 'Académica',
       list: [{
         label: 'Derechos pecuniarios 2018',
-        url: 'http://sgral.udistrital.edu.co/xdata/sgral/Derechos_Pecuniarios2018.pdf'
+        url: 'http://sgral.udistrital.edu.co/xdata/sgral/Derechos_Pecuniarios2018.pdf',
       }, {
         label: 'Estatuto Académico',
-        url: 'http://sgral.udistrital.edu.co/xdata/csu/acu_1996-004.pdf'
+        url: 'http://sgral.udistrital.edu.co/xdata/csu/acu_1996-004.pdf',
       }, {
         label: 'Estatuto Estudiantil',
-        url: 'http://sgral.udistrital.edu.co/xdata/csu/acu_1993-027.pdf'
+        url: 'http://sgral.udistrital.edu.co/xdata/csu/acu_1993-027.pdf',
       }, {
         label: 'Estatuto Docente',
-        url: 'http://sgral.udistrital.edu.co/xdata/csu/acu_2002-011.pdf'
+        url: 'http://sgral.udistrital.edu.co/xdata/csu/acu_2002-011.pdf',
       }]
     },
     {
       subtitle: 'General',
       list: [{
         label: 'Estatuto General',
-        url: 'http://sgral.udistrital.edu.co/xdata/csu/acu_1997-003.pdf'
+        url: 'http://sgral.udistrital.edu.co/xdata/csu/acu_1997-003.pdf',
       }]
     }];
     this.recomendados = [{
       label: 'Sistema de gestión ambiental.',
-      url: 'http://comunidad.udistrital.edu.co/piga/'
+      url: 'http://comunidad.udistrital.edu.co/piga/',
     }, {
       label: 'Bitácora.',
-      url: 'http://comunidad.udistrital.edu.co/bitacoraud/'
+      url: 'http://comunidad.udistrital.edu.co/bitacoraud/',
     }, {
       label: 'Distrinautas',
-      url: 'http://comunidad.udistrital.edu.co/distrinautas/'
+      url: 'http://comunidad.udistrital.edu.co/distrinautas/',
     }, {
       label: 'Elecciones UD',
-      url: 'http://comunidad.udistrital.edu.co/elecciones/'
+      url: 'http://comunidad.udistrital.edu.co/elecciones/',
     }, {
       label: 'Transparencia y acceso a la información pública',
-      url: 'https://www.udistrital.edu.co/transparencia'
+      url: 'https://www.udistrital.edu.co/transparencia',
     }, {
       label: 'Hora legal Colombiana',
-      url: 'http://horalegal.inm.gov.co/'
+      url: 'http://horalegal.inm.gov.co/',
     }, {
       label: 'SPWI anterior',
-      url: 'http://www1.udistrital.edu.co/'
+      url: 'http://www1.udistrital.edu.co/',
     }, {
       label: 'Ingreso publicadores',
-      url: 'https://www.udistrital.edu.co/user/login'
+      url: 'https://www.udistrital.edu.co/user/login',
     }];
   }
 }
