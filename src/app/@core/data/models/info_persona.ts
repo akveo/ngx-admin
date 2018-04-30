3a97dff2570fd2960a184c4af7f149b5681a7eb3
geiner <geiner.salcedo.ppc0111@gmail.com>
2018-04-30 13:02:09 -0500

    componente para informacion general de la persona

--- src/app/@core/data/models/info_persona.ts
+++ src/app/@core/data/models/info_persona.ts
@@ -1,20 +1,14 @@
 
-import { TipoDiscapacidad } from './tipo_discapacidad';
 import { EstadoCivil } from './estado_civil';
-import { GrupoEtnico } from './grupo_etnico';
 import { Genero } from './genero';
 
 export class InfoPersona {
-  Id: number;
   PrimerNombre: string;
   SegundoNombre: string;
   PrimerApellido: string;
   SegundoApellido: string;
   FechaNacimiento: Date;
-  Usuario: string;
   Foto: string;
-  TipoDiscapacidad: Array<TipoDiscapacidad>;
   EstadoCivil: EstadoCivil;
-  GrupoEtnico: GrupoEtnico;
   Genero: Genero;
 }
