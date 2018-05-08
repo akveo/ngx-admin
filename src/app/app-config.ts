d8f68b83255fd0d47226436607ab61e560318e85
Fabian León <fadarsaleeing@gmail.com>
2018-05-08 13:24:55 -0500

    limpieza de entorno, cambio de redirect

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -2,11 +2,6 @@ export const Config = {
     LOCAL: {
         NUXEO: {
             PATH: 'https://documental.udistrital.edu.co/nuxeo/',
-            AUTH: {
-                method: 'basic',
-                username: 'Administrator',
-                password: 'S1st3m4s04S=Fr331P4',
-            },
         },
         WSO2_SERVICE: 'http://jbpm.udistritaloas.edu.co:8280/services',
         CORE_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8083/v1/',
@@ -15,7 +10,6 @@ export const Config = {
         TOKEN: {
             AUTORIZATION_URL: 'https://autenticacion.udistrital.edu.co/oauth2/authorize',
             CLIENTE_ID: 'pszmROXqfec4pTShgF_fn2DAAX0a',
-            REDIRECT_URL: 'http://localhost:9000/',
             RESPONSE_TYPE: 'id_token token',
             SCOPE: 'openid email profile document',
             SIGN_OUT_URL: 'https://autenticacion.udistrital.edu.co/oidc/logout',
@@ -27,11 +21,6 @@ export const Config = {
     PROD: {
         NUXEO: {
             PATH: 'https://documental.udistrital.edu.co/nuxeo/',
-            AUTH: {
-                method: 'basic',
-                username: 'Administrator',
-                password: 'S1st3m4s04S=Fr331P4',
-            },
         },
         WSO2_SERVICE: 'http://jbpm.udistritaloas.edu.co:8280/services',
         CORE_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8083/v1/',
@@ -40,7 +29,6 @@ export const Config = {
         TOKEN: {
             AUTORIZATION_URL: 'https://autenticacion.udistrital.edu.co/oauth2/authorize',
             CLIENTE_ID: 'pszmROXqfec4pTShgF_fn2DAAX0a',
-            REDIRECT_URL: 'http://localhost:9000/',
             RESPONSE_TYPE: 'id_token token',
             SCOPE: 'openid email profile document',
             SIGN_OUT_URL: 'https://autenticacion.udistrital.edu.co/oidc/logout',
@@ -51,11 +39,6 @@ export const Config = {
     PREPROD: {
         NUXEO: {
             PATH: 'https://documental.udistrital.edu.co/nuxeo/',
-            AUTH: {
-                method: 'basic',
-                username: 'Administrator',
-                password: 'S1st3m4s04S=Fr331P4',
-            },
         },
         WSO2_SERVICE: 'http://jbpm.udistritaloas.edu.co:8280/services',
         CORE_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8083/v1/',
@@ -64,11 +47,10 @@ export const Config = {
         TOKEN: {
             AUTORIZATION_URL: 'https://autenticacion.udistrital.edu.co/oauth2/authorize',
             CLIENTE_ID: '860WlkU_AfhfieBuquBqTx4uuAYa',
-            REDIRECT_URL: window.location.href,
             RESPONSE_TYPE: 'id_token token',
             SCOPE: 'openid email profile document',
             SIGN_OUT_URL: 'https://autenticacion.udistrital.edu.co/oidc/logout',
-            SIGN_OUT_REDIRECT_URL: window.location.href,
+            SIGN_OUT_REDIRECT_URL: 'https://cliente.portaloas.udistrital.edu.co/campus_cliente_develop/index.html',
         },
         CONF_MENU_SERVICE: 'http://10.20.0.254/configuracion_api/v1/menu_opcion_padre/ArbolMenus/',
     },
