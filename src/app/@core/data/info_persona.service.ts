a2961f84f895c6c9d3197b9acc07836e2f28f6e0
Jhoan Villa <jvplanesticud@gmail.com>
2018-05-22 09:01:16 -0500

    Ajustes de funcionalidad formulario de información personal con service y autenticación + internacionalización de avisos

--- src/app/@core/data/info_persona.service.ts
+++ src/app/@core/data/info_persona.service.ts
@@ -2,18 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { GENERAL } from './../../app-config';
 
-
 const httpOptions = {
-    headers: new HttpHeaders({
-        'Accept': 'application/json',
-        'authorization': 'Bearer ' + window.localStorage.getItem('access_token'),
-    }),
-}
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
-const path = GENERAL.ENTORNO.PERSONA_SERVICE;
+const path = GENERAL.ENTORNO.INFO_PERSONA_SERVICE;
 
 @Injectable()
-export class PersonaService {
+export class InfoPersonaService {
 
     constructor(private http: HttpClient) {
     }
@@ -21,12 +17,15 @@ export class PersonaService {
     get(endpoint) {
         return this.http.get(path + endpoint);
     }
+
     post(endpoint, element) {
         return this.http.post(path + endpoint, element, httpOptions);
     }
+
     put(endpoint, element) {
-        return this.http.put(path + endpoint + '/' + element.Id, element, httpOptions);
+        return this.http.put(path + endpoint + '/', element, httpOptions); // + element.Id
     }
+
     delete(endpoint, element) {
         return this.http.delete(path + endpoint + '/' + element.Id);
     }
