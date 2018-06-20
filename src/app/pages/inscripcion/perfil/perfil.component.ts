58296d3859477aec58ce790782f5e60d7c86c0d1
salcedogeiner <geiner.salcedo.ppc0111@gmail.com>
2018-06-19 21:04:01 -0500

    agregando componentes para visualizacion separada

---
+++ src/app/pages/inscripcion/perfil/perfil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.scss'],
+})
+export class PerfilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
