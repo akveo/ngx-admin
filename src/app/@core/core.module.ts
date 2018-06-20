18a4750ec21b2d9724501a618ee2096b991b4685
Dmitry Nehaychik <4dmitr@gmail.com>
2018-06-20 18:46:49 +0300

    feat: update Nebular to rc.9 (#1741)

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,6 +1,6 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbAuthModule, NbDummyAuthProvider } from '@nebular/auth';
+import { NbAuthModule, NbDummyAuthStrategy } from '@nebular/auth';
 import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
 import { of as observableOf } from 'rxjs';
 
@@ -36,17 +36,13 @@ export class NbSimpleRoleProvider extends NbRoleProvider {
 export const NB_CORE_PROVIDERS = [
   ...DataModule.forRoot().providers,
   ...NbAuthModule.forRoot({
-    providers: {
-      email: {
-        service: NbDummyAuthProvider,
-        config: {
-          delay: 3000,
-          login: {
-            rememberMe: true,
-          },
-        },
-      },
-    },
+
+    strategies: [
+      NbDummyAuthStrategy.setup({
+        name: 'email',
+        delay: 3000,
+      }),
+    ],
     forms: {
       login: {
         socialLinks: socialLinks,
@@ -56,6 +52,7 @@ export const NB_CORE_PROVIDERS = [
       },
     },
   }).providers,
+
   NbSecurityModule.forRoot({
     accessControl: {
       guest: {
@@ -69,6 +66,7 @@ export const NB_CORE_PROVIDERS = [
       },
     },
   }).providers,
+
   {
     provide: NbRoleProvider, useClass: NbSimpleRoleProvider,
   },
