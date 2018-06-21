0379cb048e83447104b62dbffe22329b02a3cd8b
Dmitry Nehaychik <4dmitr@gmail.com>
2018-06-21 17:09:29 +0300

    feat(demo): demo version additions

--- src/app/@theme/layouts/sample/sample.layout.ts
+++ src/app/@theme/layouts/sample/sample.layout.ts
@@ -26,7 +26,7 @@ import { StateService } from '../../../@core/data/state.service';
                    responsive
                    [end]="sidebar.id === 'end'">
         <nb-sidebar-header *ngIf="currentTheme !== 'corporate'">
-          <a href="#" class="btn btn-hero-success main-btn">
+          <a href="https://github.com/akveo/nebular" target="_blank" class="btn btn-hero-success main-btn">
             <i class="ion ion-social-github"></i> <span>Support Us</span>
           </a>
         </nb-sidebar-header>
