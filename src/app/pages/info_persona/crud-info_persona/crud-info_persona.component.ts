aaa12ffaeb5ab01355c5e69ef3c2fcb2cc62d23d
Mafe Avendaño <mafe.ud@gmail.com>
2018-06-28 17:01:21 -0500

    Carga del tipo y número de identificación en formulario de datos básicos

--- src/app/pages/info_persona/crud-info_persona/crud-info_persona.component.ts
+++ src/app/pages/info_persona/crud-info_persona/crud-info_persona.component.ts
@@ -118,7 +118,7 @@ export class CrudInfoPersonaComponent implements OnInit {
   public loadInfoPersona(): void {
     if (this.info_persona_id !== undefined && this.info_persona_id !== 0 &&
       this.info_persona_id.toString() !== '') {
-      this.campusMidService.get('persona/ConsultaPersona/' + this.autenticationService.getPayload().sub)
+      this.campusMidService.get('persona/ConsultaPersona/?userid=' + this.autenticationService.getPayload().sub)
         .subscribe(res => {
           if (res !== null) {
             const temp = <InfoPersona>res;
