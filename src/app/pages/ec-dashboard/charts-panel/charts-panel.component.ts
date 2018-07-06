e1ddca091ab1c540a5b2870c607191edcb6faf6f
Denis Strigo <d.strigo@akveo.com>
2018-07-06 15:42:08 +0300

    feat(ecommerce): update order chart service

--- src/app/pages/ec-dashboard/charts-panel/charts-panel.component.ts
+++ src/app/pages/ec-dashboard/charts-panel/charts-panel.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import { OrdersChartComponent } from './charts/orders-chart.component';
 import { ProfitChartComponent } from './charts/profit-chart.component';
+import { OrdersChartService, OrdersChart } from '../../../@core/data/orders-chart.service';
 
 @Component({
   selector: 'ngx-ec-charts',
@@ -9,11 +10,17 @@ import { ProfitChartComponent } from './charts/profit-chart.component';
 })
 export class EcChartsPanelComponent {
   period: string = 'week';
+  ordersChartData: OrdersChart;
 
   @ViewChild('ordersChart') ordersChart: OrdersChartComponent;
   @ViewChild('profitChart') profitChart: ProfitChartComponent;
 
+  constructor(private ordersChartService: OrdersChartService) {
+    this.getOrdersChartData(this.period);
+  }
+
   setPeriod(value: string): void {
+    this.getOrdersChartData(value);
     this.period = value;
   }
 
@@ -23,5 +30,12 @@ export class EcChartsPanelComponent {
     } else {
       this.ordersChart.resizeChart();
     }
+  }
+
+  getOrdersChartData(period: string) {
+    this.ordersChartService.getOrdersChartData(period)
+      .subscribe(ordersChartData => {
+        this.ordersChartData = ordersChartData;
+      });
   }
 }
