0a5a7cff89f9018beb56fc77512cc1b850484669
Fabián León <fadarsaleeing@gmail.com>
2018-07-30 19:31:33 -0500

    añadiendo cabecera a get de menu

--- src/app/@core/data/menu.service.ts
+++ src/app/@core/data/menu.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { GENERAL } from './../../app-config';
 
 const path = GENERAL.ENTORNO.CONF_MENU_SERVICE;
 
+const httpOptions = {
+    headers: new HttpHeaders({
+        'Accept': 'application/json',
+        'authorization': 'Bearer ' + window.localStorage.getItem('access_token'),
+    }),
+}
 @Injectable()
 export class MenuService {
   constructor(private http: HttpClient) {
@@ -17,6 +23,6 @@ export class MenuService {
       TipoOpcion: string;
       Opciones: Response[];
     }
-    return this.http.get<Response[]>(path + endpoint);
+    return this.http.get<Response[]>(path + endpoint, httpOptions);
   }
 }
