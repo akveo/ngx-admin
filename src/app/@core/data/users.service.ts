a1bd93f8ca8fdc81006840c9c5ebb96847bcf6ef
fabianleon <fadarsaleeing@gmail.com>
2018-08-13 11:13:23 -0500

    mejorando caracteristicas

--- src/app/@core/data/users.service.ts
+++ src/app/@core/data/users.service.ts
@@ -1,38 +1,39 @@
 
 import { of as observableOf, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { GENERAL } from './../../app-config';
+
+const httpOptions = {
+    headers: new HttpHeaders({
+        'Accept': 'application/json',
+        'authorization': 'Bearer ' + window.localStorage.getItem('access_token'),
+    }),
+}
 
 
-let counter = 0;
+const path = GENERAL.ENTORNO.CAMPUS_MID;
 
 @Injectable()
 export class UserService {
 
-  private users = {
-    nick: { name: 'Nick Jones', picture: 'assets/images/nick.png' },
-    eva: { name: 'Eva Moor', picture: 'assets/images/eva.png' },
-    jack: { name: 'Jack Williams', picture: 'assets/images/jack.png' },
-    lee: { name: 'Lee Wong', picture: 'assets/images/lee.png' },
-    alan: { name: 'Alan Thompson', picture: 'assets/images/alan.png' },
-    kate: { name: 'Kate Martinez', picture: 'assets/images/kate.png' },
-  };
+  private users = {};
 
-  private userArray: any[];
-
-  constructor() {
-    // this.userArray = Object.values(this.users);
+  constructor(private http: HttpClient) {
+    if (window.localStorage.getItem('id_token') !== null && window.localStorage.getItem('id_token') !== undefined) {
+      const id_token = window.localStorage.getItem('id_token').split('.');
+      const payload = JSON.parse(atob(id_token[1]));
+      this.http.get(path + 'persona/?query=Usuario:' + payload.sub)
+      .subscribe(res => {
+        if (res !== null) {
+          this.users = res;
+        }
+      });
+    }
   }
-
-  getUsers(): Observable<any> {
+  
+  getUser(): Observable<any> {
     return observableOf(this.users);
   }
-
-  getUserArray(): Observable<any[]> {
-    return observableOf(this.userArray);
-  }
-
-  getUser(): Observable<any> {
-    counter = (counter + 1) % this.userArray.length;
-    return observableOf(this.userArray[counter]);
-  }
+  
 }
