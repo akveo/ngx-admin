5dc1fe1a2e1ce71d17fa18c63502c124428dcc98
geiner <geiner.salcedo.ppc0111@gmail.com>
2018-08-15 12:12:40 -0500

    ajuste a componentes de view datos contacto, datos caracteristicos e info persona, para visualizar la informacion exacta al formulario y sobre ajustes en los servicios

--- src/app/pages/info_caracteristica/view-info_caracteristica/view-info_caracteristica.component.ts
+++ src/app/pages/info_caracteristica/view-info_caracteristica/view-info_caracteristica.component.ts
@@ -29,7 +29,7 @@ export class ViewInfoCaracteristicaComponent implements OnInit {
   public loadInfoCaracteristica(): void {
     if (this.info_caracteristica_id !== undefined && this.info_caracteristica_id !== 0 &&
       this.info_caracteristica_id.toString() !== '') {
-      this.campusMidService.get('/persona/DatosComplementarios/' + this.info_caracteristica_id)
+      this.campusMidService.get('/persona/DatosComplementarios/' + this.info_caracteristica_id + '?query=TipoRelacionUbicacionEnte.CodigoAbreviacion:LN')
         .subscribe(res => {
           const r = <any>res;
           if (r !== null && r.Type !== 'error') {
