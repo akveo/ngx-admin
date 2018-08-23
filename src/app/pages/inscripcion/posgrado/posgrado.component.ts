487579cce60119010aaa3c34e40c745ee1fe7681
fabianleon <fadarsaleeing@gmail.com>
2018-08-23 15:53:16 -0500

    lint

--- src/app/pages/inscripcion/posgrado/posgrado.component.ts
+++ src/app/pages/inscripcion/posgrado/posgrado.component.ts
@@ -175,18 +175,15 @@ export class PosgradoComponent implements OnInit {
   public captureScreen() {
     const data = document.getElementById('contentToConvert');
     html2canvas(data).then(canvas => {
-      // Few necessary setting options  
       const imgWidth = 208;
-      const pageHeight = 295;
-      const imgHeight = canvas.height * imgWidth / canvas.width;
-      const heightLeft = imgHeight;
-
-      const contentDataURL = canvas.toDataURL('image/png')
-      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      // const pageHeight = 295;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      // const heightLeft = imgHeight;
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jspdf('p', 'mm', 'a4');
       const position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
-      pdf.save('MYPdf.pdf'); // Generated PDF   
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('MYPdf.pdf');
     });
   }
-
 }
