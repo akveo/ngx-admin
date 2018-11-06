9a27d9f5811ae8c82eed14e7955f8e806c9a4c76
salcedogeiner <geiner.salcedo.ppc0111@gmail.com>
2018-11-05 19:21:54 -0500

    ajustes error y rxj

--- src/app/@core/data/admisiones.service.ts
+++ src/app/@core/data/admisiones.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Config } from './../../app-config';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 
 const httpOptions = {
@@ -17,15 +19,39 @@ export class AdmisionesService {
     }
 
     get(endpoint) {
-        return this.http.get(path + endpoint);
+        return this.http.get(path + endpoint).pipe(
+          catchError(this.handleError)
+        );
       }
     post(endpoint, element) {
-        return this.http.post(path + endpoint, element, httpOptions);
+        return this.http.post(path + endpoint, element, httpOptions).pipe(
+          catchError(this.handleError)
+        );
     }
     put(endpoint, element) {
-        return this.http.put(path + endpoint + '/' + element.Id, element, httpOptions);
+        return this.http.put(path + endpoint + '/' + element.Id, element, httpOptions).pipe(
+          catchError(this.handleError)
+        );
     }
     delete(endpoint, element) {
-        return this.http.delete(path + endpoint + '/' + element.Id);
+        return this.http.delete(path + endpoint + '/' + element.Id).pipe(
+          catchError(this.handleError)
+        );
     }
+
+    private handleError(error: HttpErrorResponse) {
+      if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error.message);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      }
+      // return an observable with a user-facing error message
+      return throwError(
+        'Something bad happened; please try again later.');
+    };
 }
