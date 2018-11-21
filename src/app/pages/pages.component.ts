ba37f52709b80726bd854e0fa4520f3c633b6878
denStrigo <d.strigo@akveo.com>
2018-11-21 13:45:52 +0300

    fix(icons popover): fix eva icons color, popover arrow style

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -4,6 +4,7 @@ import { MENU_ITEMS } from './pages-menu';
 
 @Component({
   selector: 'ngx-pages',
+  styleUrls: ['pages.component.scss'],
   template: `
     <ngx-sample-layout>
       <nb-menu [items]="menu"></nb-menu>
