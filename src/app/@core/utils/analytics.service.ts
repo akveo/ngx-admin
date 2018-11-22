c019de67c6cee388ecd299ff1545f39e073d5f21
denStrigo <d.strigo@akveo.com>
2018-11-22 18:14:43 +0300

    fix(analytics): update analytics service, add detection if google analytics was loaded

--- src/app/@core/utils/analytics.service.ts
+++ src/app/@core/utils/analytics.service.ts
@@ -28,8 +28,14 @@ export class AnalyticsService {
   }
 
   trackEvent(eventName: string, eventVal: string = '') {
-    if (this.enabled) {
+    if (this.enabled && this.window.ga) {
       ga('send', 'event', eventName, eventVal);
+    }
+
+    if (!this.window.ga) {
+      setTimeout(() => {
+        this.trackEvent(eventName, eventVal);
+      }, 500);
     }
   }
 }
