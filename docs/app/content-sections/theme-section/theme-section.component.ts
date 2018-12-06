0e0022ad3460fe16a3cb693124e44770988f85f1
denStrigo <d.strigo@akveo.com>
2018-12-06 20:14:15 +0300

    feat(landing): add carousel for theme section, update links

--- docs/app/content-sections/theme-section/theme-section.component.ts
+++ docs/app/content-sections/theme-section/theme-section.component.ts
@@ -5,6 +5,7 @@
  */
 
 import { Component } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'ngx-landing-theme-section',
@@ -13,4 +14,42 @@ import { Component } from '@angular/core';
 })
 export class ThemeSectionComponent {
 
+  private themes: string[] = [
+    'Light',
+    'Cosmic',
+    'Corporate',
+  ];
+
+  sliderIndex: number = 1;
+  swiperConfig: SwiperConfigInterface = {
+    direction: 'horizontal',
+    spaceBetween: 200,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    keyboard: true,
+    navigation: true,
+
+    effect: 'coverflow',
+    grabCursor: true,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 500,
+      modifier: 1,
+      slideShadows : false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      hideOnClick: false,
+      renderBullet: (index, className) => {
+        return `
+          <span class="${className}">
+            <span>
+              ${this.themes[index]}
+            </span>
+          </span>`;
+      },
+    },
+  };
 }
