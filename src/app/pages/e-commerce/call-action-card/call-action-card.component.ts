5a4fdb486ff9beb6f425b7c61bcebbb0645bdefa
u.ahmetvaliyev <ahmetvaliev.vladislav@gmail.com>
2019-01-04 14:52:51 +0300

    feat(dashboard): ngx dashboard restyling, sidebar restyling

---
+++ src/app/pages/e-commerce/call-action-card/call-action-card.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, OnDestroy } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+import { takeWhile } from 'rxjs/operators';
+
+@Component({
+  selector: 'ngx-call-action-card',
+  styleUrls: ['./call-action-card.component.scss'],
+  template: `
+    <nb-card>
+      <div class="icon-container hidden-sm-down">
+        <div class="icon" [innerHTML]="type | eva">
+        </div>
+      </div>
+
+      <div class="details">
+        <div class="title">{{ title }}</div>
+        <ng-content></ng-content>
+      </div>
+      <div class="actions">
+        <a nbButton size="large" hero href="{{ link }}" [ngClass]="{
+              'btn-primary': currentTheme === 'cosmic',
+              'btn-warning': currentTheme === 'corporate',
+              'btn-danger': currentTheme === 'default'
+        }">{{ linkTitle }}</a>
+      </div>
+    </nb-card>
+  `,
+})
+export class CallActionCardComponent implements OnDestroy {
+
+  @Input() title: string;
+  @Input() type: string;
+  @Input() link: string;
+  @Input() linkTitle: string;
+  private alive = true;
+  currentTheme: string;
+  constructor(private themeService: NbThemeService) {
+    this.themeService.getJsTheme()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(theme => {
+        this.currentTheme = theme.name;
+      });
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
+}
