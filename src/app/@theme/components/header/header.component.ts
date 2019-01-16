a671428adb8c9f5122bac8af6d0f8401a12074ea
u.ahmetvaliyev <ahmetvaliev.vladislav@gmail.com>
2019-01-16 13:58:23 +0300

    feat(demo): currentTheme check removed, header fonts fix

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { NbMenuService, NbSidebarService, NbThemeService } from '@nebular/theme';
 import { UserService } from '../../../@core/data/users.service';
-import { takeWhile } from 'rxjs/operators/takeWhile';
 import { AnalyticsService } from '../../../@core/utils';
 import { LayoutService } from '../../../@core/utils';
 
@@ -19,23 +18,11 @@ export class HeaderComponent implements OnInit , OnDestroy {
 
   userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
 
-  private alive = true;
-
-  currentTheme: string;
-
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
               private userService: UserService,
               private analytics: AnalyticsService,
-              private layoutService: LayoutService,
-              private themeService: NbThemeService) {
-
-    this.themeService.getJsTheme()
-      .pipe(takeWhile(() => this.alive))
-      .subscribe(theme => {
-        this.currentTheme = theme.name;
-      });
-  }
+              private layoutService: LayoutService) {}
 
   ngOnInit() {
     this.userService.getUsers()
