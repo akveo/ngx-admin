4c82e7605e81bf6cf10719ee7bf45907bb234a9b
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-06-19 21:13:11 +0300

    refactor(ui features): enable module route

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -28,11 +28,11 @@ const routes: Routes = [{
       loadChildren: () => import('./forms/forms.module')
         .then(m => m.FormsModule),
     },
-    // {
-    //   path: 'ui-features',
-    //   loadChildren: () => import('./ui-features/ui-features.module')
-    //     .then(m => m.UiFeaturesModule),
-    // },
+    {
+      path: 'ui-features',
+      loadChildren: () => import('./ui-features/ui-features.module')
+        .then(m => m.UiFeaturesModule),
+    },
     // {
     //   path: 'modal-overlays',
     //   loadChildren: () => import('./modal-overlays/modal-overlays.module')
