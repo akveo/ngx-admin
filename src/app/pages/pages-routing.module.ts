5060c2b2280b05d124efe713b3851cfbf119f298
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-06-20 11:36:30 +0300

    refactor(extra components): enable module route

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -38,11 +38,11 @@ const routes: Routes = [{
       loadChildren: () => import('./modal-overlays/modal-overlays.module')
         .then(m => m.ModalOverlaysModule),
     },
-    // {
-    //   path: 'extra-components',
-    //   loadChildren: () => import('./extra-components/extra-components.module')
-    //     .then(m => m.ExtraComponentsModule),
-    // },
+    {
+      path: 'extra-components',
+      loadChildren: () => import('./extra-components/extra-components.module')
+        .then(m => m.ExtraComponentsModule),
+    },
     // {
     //   path: 'maps',
     //   loadChildren: () => import('./maps/maps.module')
