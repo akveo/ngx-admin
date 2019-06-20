55aa6848f2070f00f4473a23367ffc85ae0af49e
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-06-20 13:35:07 +0300

    refactor: enable dashboard redirect

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -68,11 +68,11 @@ const routes: Routes = [{
     //   loadChildren: () => import('./miscellaneous/miscellaneous.module')
     //     .then(m => m.MiscellaneousModule),
     // },
-    // {
-    //   path: '',
-    //   redirectTo: 'dashboard',
-    //   pathMatch: 'full',
-    // },
+    {
+      path: '',
+      redirectTo: 'dashboard',
+      pathMatch: 'full',
+    },
     {
       path: '**',
       component: NotFoundComponent,
