82f0022acebac86ca21367d683bb4b4e51e9107a
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-06-21 22:59:24 +0300

    feat: add dark js theme

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -37,6 +37,7 @@ import {
 import { DEFAULT_THEME } from './styles/theme.default';
 import { COSMIC_THEME } from './styles/theme.cosmic';
 import { CORPORATE_THEME } from './styles/theme.corporate';
+import { DARK_THEME } from './styles/theme.dark';
 
 const NB_MODULES = [
   NbLayoutModule,
@@ -83,7 +84,7 @@ export class ThemeModule {
           {
             name: 'default',
           },
-          [ DEFAULT_THEME, COSMIC_THEME, CORPORATE_THEME ],
+          [ DEFAULT_THEME, COSMIC_THEME, CORPORATE_THEME, DARK_THEME ],
         ).providers,
       ],
     };
