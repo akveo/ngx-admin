c2890a46d214dae9a6eee481e9035b50320de672
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-06-24 16:28:21 +0300

    refactor(layout): enable window mode

--- src/app/@theme/layouts/one-column/one-column.layout.ts
+++ src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-one-column-layout',
   styleUrls: ['./one-column.layout.scss'],
   template: `
-    <nb-layout center>
+    <nb-layout windowMode>
       <nb-layout-header fixed>
         <ngx-header></ngx-header>
       </nb-layout-header>
