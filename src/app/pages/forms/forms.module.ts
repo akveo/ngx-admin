77066ad20cdf0bc0fb2e93a7cfa255a6600d1903
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-06-24 17:38:19 +0300

    refactor: update buttons example

--- src/app/pages/forms/forms.module.ts
+++ src/app/pages/forms/forms.module.ts
@@ -3,9 +3,11 @@ import {
   NbActionsModule,
   NbButtonModule,
   NbCardModule,
-  NbCheckboxModule, NbDatepickerModule,
+  NbCheckboxModule,
+  NbDatepickerModule, NbIconModule,
   NbInputModule,
-  NbRadioModule, NbSelectModule,
+  NbRadioModule,
+  NbSelectModule,
   NbUserModule,
 } from '@nebular/theme';
 
@@ -16,14 +18,6 @@ import { FormInputsComponent } from './form-inputs/form-inputs.component';
 import { FormLayoutsComponent } from './form-layouts/form-layouts.component';
 import { DatepickerComponent } from './datepicker/datepicker.component';
 import { ButtonsComponent } from './buttons/buttons.component';
-import { DefaultButtonsComponent } from './buttons/default-buttons/default-buttons.component';
-import { OutlineButtonsComponent } from './buttons/outline-buttons/outline-buttons.component';
-import { HeroButtonComponent } from './buttons/hero-buttons/hero-buttons.component';
-import { ShapeButtonsComponent } from './buttons/shape-buttons/shape-buttons.component';
-import { SizeButtonsComponent } from './buttons/size-buttons/size-buttons.component';
-import { ActionGroupsComponent } from './buttons/action-groups/action-groups.component';
-import { LabeledActionsGroupComponent } from './buttons/labeled-actions-group/labeled-actions-group.component';
-import { ButtonElementsComponent } from './buttons/button-elements/button-elements.component';
 
 @NgModule({
   imports: [
@@ -38,18 +32,11 @@ import { ButtonElementsComponent } from './buttons/button-elements/button-elemen
     NbDatepickerModule,
     FormsRoutingModule,
     NbSelectModule,
+    NbIconModule,
   ],
   declarations: [
     FormsComponent,
     ButtonsComponent,
-    DefaultButtonsComponent,
-    OutlineButtonsComponent,
-    HeroButtonComponent,
-    ShapeButtonsComponent,
-    SizeButtonsComponent,
-    ActionGroupsComponent,
-    LabeledActionsGroupComponent,
-    ButtonElementsComponent,
     FormInputsComponent,
     FormLayoutsComponent,
     DatepickerComponent,
