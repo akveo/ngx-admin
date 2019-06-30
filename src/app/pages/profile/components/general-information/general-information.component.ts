f56a49455b88a31a99cf6ef60c60d11bbb5f206c
Leon <leon.lafa@gmail.com>
2019-06-30 17:38:33 +0100

    feat: amendment for code review

--- src/app/pages/profile/components/general-information/general-information.component.ts
+++ src/app/pages/profile/components/general-information/general-information.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { User } from '../../../../@core/data/users';
 
 @Component({
   selector: 'ngx-general-information',
@@ -8,14 +9,19 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 })
 export class GeneralInformationComponent implements OnInit {
 
-  @Input() user: any;
+  @Input() user: User;
   file: File;
   originalProfilePic = true;
   class = 'hide-text';
+  firstName: string;
+  lastName: string;
 
   constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+  const names: string [] = this.user.name.split(' ');
+  this.firstName = names[0];
+  this.lastName = names[1];
   }
 
   getFilePath(e: any) {
