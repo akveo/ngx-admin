e32ade27f06a2c3be94cb6c08c8d391d28ddb6a8
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-07-15 13:58:20 +0300

    fix(404 page): use Nebular button

--- src/app/pages/miscellaneous/miscellaneous.module.ts
+++ src/app/pages/miscellaneous/miscellaneous.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { NbCardModule } from '@nebular/theme';
+import { NbButtonModule, NbCardModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { MiscellaneousRoutingModule } from './miscellaneous-routing.module';
@@ -10,6 +10,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
   imports: [
     ThemeModule,
     NbCardModule,
+    NbButtonModule,
     MiscellaneousRoutingModule,
   ],
   declarations: [
