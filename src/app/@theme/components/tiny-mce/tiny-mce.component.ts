2dd7e4a6e404200972616ac3f8871632861df109
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-08-23 19:10:29 +0300

    fix(tiny mce): set absolute path for skin url

--- src/app/@theme/components/tiny-mce/tiny-mce.component.ts
+++ src/app/@theme/components/tiny-mce/tiny-mce.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, AfterViewInit, Output, EventEmitter, ElementRef } from '@angular/core';
+import { LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'ngx-tiny-mce',
@@ -10,13 +11,16 @@ export class TinyMCEComponent implements OnDestroy, AfterViewInit {
 
   editor: any;
 
-  constructor(private host: ElementRef) { }
+  constructor(
+    private host: ElementRef,
+    private locationStrategy: LocationStrategy,
+  ) { }
 
   ngAfterViewInit() {
     tinymce.init({
       target: this.host.nativeElement,
       plugins: ['link', 'paste', 'table'],
-      skin_url: 'assets/skins/lightgray',
+      skin_url: `${this.locationStrategy.getBaseHref()}assets/skins/lightgray`,
       setup: editor => {
         this.editor = editor;
         editor.on('keyup', () => {
