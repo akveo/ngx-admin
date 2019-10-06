aa311f96b556ab9fd59e7f5bf3d177af09f730c8
Leon <leon.lafa@gmail.com>
2019-10-07 00:07:26 +0100

    fix:menu-not-collapsing
    Resolve menu not collapsing
    
    #5500

--- src/app/@theme/layouts/one-column/one-column.layout.ts
+++ src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -1,26 +1,35 @@
 import { Component } from '@angular/core';
-
+import { NbSidebarService } from '@nebular/theme';
 @Component({
   selector: 'ngx-one-column-layout',
   styleUrls: ['./one-column.layout.scss'],
   template: `
-    <nb-layout windowMode>
-      <nb-layout-header fixed>
-        <ngx-header></ngx-header>
-      </nb-layout-header>
-
-      <nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive>
-        <ng-content select="nb-menu"></ng-content>
-      </nb-sidebar>
-
-      <nb-layout-column>
-        <ng-content select="router-outlet"></ng-content>
-      </nb-layout-column>
-
-      <nb-layout-footer fixed>
-        <ngx-footer></ngx-footer>
-      </nb-layout-footer>
-    </nb-layout>
-  `,
+    <nb-layout windowMode>
+      <nb-layout-header fixed>
+        <ngx-header></ngx-header>
+      </nb-layout-header>
+      <nb-sidebar class="menu-sidebar" tag="menu-sidebar" (click)="clickMenu($event)" responsive>
+        <ng-content select="nb-menu"></ng-content>
+      </nb-sidebar>
+      <nb-layout-column (click)="clickOutside()">
+        <ng-content select="router-outlet"></ng-content>
+      </nb-layout-column>
+      <nb-layout-footer fixed (click)="clickOutside()">
+        <ngx-footer></ngx-footer>
+      </nb-layout-footer>
+    </nb-layout>
+  `,
 })
-export class OneColumnLayoutComponent {}
+export class OneColumnLayoutComponent {
+
+  constructor(
+    private sidebarService: NbSidebarService) { }
+
+  clickOutside() {
+    this.sidebarService.toggle(true, 'menu-sidebar');
+  }
+
+  clickMenu(e) {
+    if (e.target.className === 'menu-title') this.sidebarService.toggle(true, 'menu-sidebar');
+  }
+}
