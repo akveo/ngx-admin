76d31dadbc0dcd02aa06de03d0b38a74230a1899
Vladimir Lugovsky <lugovsky.v@gmail.com>
2019-12-20 11:09:37 +0300

    feat(seo): add canonical tag (#5578)

--- src/app/@core/utils/index.ts
+++ src/app/@core/utils/index.ts
@@ -2,10 +2,12 @@ import { LayoutService } from './layout.service';
 import { AnalyticsService } from './analytics.service';
 import { PlayerService } from './player.service';
 import { StateService } from './state.service';
+import { SeoService } from './seo.service';
 
 export {
   LayoutService,
   AnalyticsService,
   PlayerService,
+  SeoService,
   StateService,
 };
