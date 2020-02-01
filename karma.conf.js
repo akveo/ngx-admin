3e12e5e107d03d4ee1cffc30f58410802a40741d
joshtune <josh@eitacode.com>
2020-02-01 11:58:07 -1000

    Get e2e and unit tests to work

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,12 @@ module.exports = function (config) {
         base: 'ChromeHeadless',
         flags: [
           '--no-sandbox',
-          '--no-watch'
+          '--no-watch',
+          '--no-progress',
+          '--no-proxy-server',
+          '--disable-web-security',
+          '--disable-gpu',
+          '--js-flags=--max-old-space-size=8196'
         ]
       }
     },
