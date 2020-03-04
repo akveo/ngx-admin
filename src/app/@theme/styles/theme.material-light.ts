1da33582672b6b894e7f5c9ffeb7e658c27cb6a7
eugene-sinitsyn <e.sinitsyn.s@akveo.com>
2020-03-04 13:29:30 +0300

    Move material theme files from @nebular

--- src/app/@theme/styles/theme.material-light.ts
+++ src/app/@theme/styles/theme.material-light.ts
@@ -1,4 +1,51 @@
-import { NbJSThemeOptions, MATERIAL_LIGHT_THEME as baseTheme } from '@nebular/theme';
+import { NbJSThemeOptions } from '@nebular/theme';
+
+const palette = {
+  primary: '#6200ee',
+  success: '#60af20',
+  info: '#0495ee',
+  warning: '#ff9f05',
+  danger: '#b00020',
+};
+
+export const baseTheme: NbJSThemeOptions = {
+  name: 'material-light',
+  base: 'default',
+  variables: {
+    fontMain: 'Roboto, sans-serif',
+    fontSecondary: 'Roboto, sans-serif',
+
+    bg: '#ffffff',
+    bg2: '#f5f5f5',
+    bg3: '#ebebeb',
+    bg4: '#e0e0e0',
+
+    border: '#ffffff',
+    border2: '#f5f5f5',
+    border3: '#ebebeb',
+    border4: '#e0e0e0',
+    border5: '#b3b3b3',
+
+    fg: '#838383',
+    fgHeading: '#1a2138', // ?
+    fgText: '#1a2138', // ?
+    fgHighlight: palette.primary,
+    layoutBg: '#ebebeb',
+    separator: '#ebebeb',
+
+    primary: palette.primary,
+    success: palette.success,
+    info: palette.info,
+    warning: palette.warning,
+    danger: palette.danger,
+
+    primaryLight: '#903df4',
+    successLight: '#8fcf50',
+    infoLight: '#40bbf4',
+    warningLight: '#ffbe43',
+    dangerLight: '#cf3341',
+  },
+};
 
 const baseThemeVariables = baseTheme.variables;
 
