eaf4143731df5a267ccef1369648b15d73883b36
eugene-sinitsyn <e.sinitsyn.s@akveo.com>
2020-03-06 14:21:17 +0300

    Configure material theme, add material button and input examples

--- src/app/pages/forms/buttons/buttons.component.ts
+++ src/app/pages/forms/buttons/buttons.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NbComponentShape, NbComponentSize, NbComponentStatus } from '@nebular/theme';
+import { NbComponentShape, NbComponentSize, NbComponentStatus, NbThemeService } from '@nebular/theme';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ngx-buttons',
@@ -7,7 +8,23 @@ import { NbComponentShape, NbComponentSize, NbComponentStatus } from '@nebular/t
   templateUrl: './buttons.component.html',
 })
 export class ButtonsComponent {
-  statuses: NbComponentStatus[] = [ 'primary', 'success', 'info', 'warning', 'danger' ];
-  shapes: NbComponentShape[] = [ 'rectangle', 'semi-round', 'round' ];
-  sizes: NbComponentSize[] = [ 'tiny', 'small', 'medium', 'large', 'giant' ];
+  public constructor(private readonly themeService: NbThemeService) {}
+
+  private readonly subscription: Subscription = new Subscription();
+
+  public readonly statuses: NbComponentStatus[] = [ 'primary', 'success', 'info', 'warning', 'danger' ];
+  public readonly shapes: NbComponentShape[] = [ 'rectangle', 'semi-round', 'round' ];
+  public readonly sizes: NbComponentSize[] = [ 'tiny', 'small', 'medium', 'large', 'giant' ];
+  public materialThemeActivated: boolean = false;
+
+  public ngOnInit(): void {
+    this.subscription.add(this.themeService.onThemeChange().subscribe(theme => {
+      const themeName: string = theme?.name || '';
+      this.materialThemeActivated = themeName.startsWith('material');
+    }));
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/pages/forms/form-inputs/form-inputs.component.ts
+++ src/app/pages/forms/form-inputs/form-inputs.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ngx-form-inputs',
   styleUrls: ['./form-inputs.component.scss'],
   templateUrl: './form-inputs.component.html',
 })
-export class FormInputsComponent {
+export class FormInputsComponent implements OnInit, OnDestroy {
+  public constructor(private readonly themeService: NbThemeService) {}
 
-  starRate = 2;
-  heartRate = 4;
-  radioGroupValue = 'This is value 2';
+  private readonly subscription: Subscription = new Subscription();
+
+  public materialThemeActivated: boolean = false;
+  public starRate: number = 2;
+  public heartRate: number = 4;
+  public radioGroupValue: string = 'This is value 2';
+
+  public ngOnInit(): void {
+    this.subscription.add(this.themeService.onThemeChange().subscribe(theme => {
+      const themeName: string = theme?.name || '';
+      this.materialThemeActivated = themeName.startsWith('material');
+    }));
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
