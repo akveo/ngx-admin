2b768b019134d214203c1194c4a65b6762e90ce2
eugene-sinitsyn <e.sinitsyn.s@akveo.com>
2020-03-12 18:09:23 +0300

    Update demo links

--- docs/app/pages/home/theme-section/theme-section.component.ts
+++ docs/app/pages/home/theme-section/theme-section.component.ts
@@ -4,7 +4,8 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, Input } from '@angular/core';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 import { takeWhile } from 'rxjs/operators';
 import {
@@ -21,6 +22,7 @@ import {
 export class ThemeSectionComponent implements OnDestroy {
 
   private alive = true;
+  private forMaterialTheme: boolean = false;
   private themes: string[] = [
     'Material\nLight',
     'Material\nDark',
@@ -66,6 +68,50 @@ export class ThemeSectionComponent implements OnDestroy {
   swiperConfig: SwiperConfigInterface  = {
     ...this.initialSwiperConfig,
   };
+
+  @Input() public set material(value: any) {
+    this.forMaterialTheme = coerceBooleanProperty(value);
+  }
+
+  public get iconColor(): string {
+    return this.forMaterialTheme ? '#6200ee' : '#00db92';
+  }
+
+  public get materialLightDemoUrl(): string {
+    return this.forMaterialTheme
+      ? 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=material-light&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_material_themes_material_light'
+      : 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=material-light&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_landing_themes_material_light';
+  }
+
+  public get materialDarkDemoUrl(): string {
+    return this.forMaterialTheme
+      ? 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=material-dark&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_material_themes_material_dark'
+      : 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=material-dark&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_landing_themes_material_dark';
+  }
+
+  public get lightDemoUrl(): string {
+    return this.forMaterialTheme
+      ? 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=default&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_material_themes_default'
+      : 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=default&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_landing_themes_default';
+  }
+
+  public get darkDemoUrl(): string {
+    return this.forMaterialTheme
+      ? 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=dark&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_material_themes_dark'
+      : 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=dark&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_landing_themes_dark';
+  }
+
+  public get cosmicDemoUrl(): string {
+    return this.forMaterialTheme
+      ? 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=cosmic&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_material_themes_cosmic'
+      : 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=cosmic&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_landing_themes_cosmic';
+  }
+
+  public get corporateDemoUrl(): string {
+    return this.forMaterialTheme
+      ? 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=corporate&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_material_themes_corporate'
+      : 'https://www.akveo.com/ngx-admin/pages/dashboard?theme=corporate&utm_campaign=ngx_admin%20-%20demo%20-%20ngx_admin%20docs&utm_source=ngx_admin&utm_medium=referral&utm_content=ngx_admin_landing_themes_corporate';
+  }
 
   constructor(
     private themeService: NbThemeService,
