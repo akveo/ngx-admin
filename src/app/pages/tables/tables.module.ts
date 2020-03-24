54ac81d95472156d668e2dc8d5adb53574971a4f
Evgeny Lupanov <e.lupanov@akveo.com>
2020-03-24 17:36:32 +0300

    feat: update ng2-smart-table to 1.6

--- src/app/pages/tables/tables.module.ts
+++ src/app/pages/tables/tables.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { NbCardModule, NbIconModule, NbInputModule, NbTreeGridModule } from '@nebular/theme';
-// import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { TablesRoutingModule, routedComponents } from './tables-routing.module';
@@ -14,7 +14,7 @@ import { FsIconComponent } from './tree-grid/tree-grid.component';
     NbInputModule,
     ThemeModule,
     TablesRoutingModule,
-    // Ng2SmartTableModule,
+    Ng2SmartTableModule,
   ],
   declarations: [
     ...routedComponents,
