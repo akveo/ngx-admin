188f12f63c4e1a546671ae306a30cee3ea1f0198
Evgeny Lupanov <e.lupanov@akveo.com>
2020-03-26 11:14:44 +0300

    fix: docs build

--- docs/app/@core/core.module.ts
+++ docs/app/@core/core.module.ts
@@ -29,8 +29,8 @@ export class CoreModule {
     throwIfAlreadyLoaded(parentModule, 'CoreModule');
   }
 
-  static forRoot(): ModuleWithProviders {
-    return <ModuleWithProviders>{
+  static forRoot(): ModuleWithProviders<CoreModule> {
+    return {
       ngModule: CoreModule,
       providers: [
         ...NB_CORE_PROVIDERS,
