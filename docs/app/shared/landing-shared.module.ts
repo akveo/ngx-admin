2a921c2a0f08636c2522d561ed0836acf4345505
Alex <alexey.dred@gmail.com>
2020-04-01 11:51:28 +0300

    fix(docs): header fonts and material theme link

--- docs/app/shared/landing-shared.module.ts
+++ docs/app/shared/landing-shared.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { NbPopoverModule } from '@nebular/theme';
+import {NbPopoverModule} from '@nebular/theme';
 import { MaterialThemeLinkComponent } from './components/material-theme-link/material-theme-link.component';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
 import { EvaIconsPipe } from './pipes/eva-icons.pipe';
