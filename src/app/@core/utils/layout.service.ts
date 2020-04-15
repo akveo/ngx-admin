2d7544197703a6c34c8553b0b66a5c5c43b03ecb
sashaqred <sashaqred@ya.ru>
2020-04-15 11:42:52 +0300

    refactor: improve observable layout change sharing

---
+++ src/app/@core/utils/layout.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { delay, shareReplay } from 'rxjs/operators';
+
+@Injectable()
+export class LayoutService {
+
+  protected layoutSize$ = new Subject();
+  protected layoutSizeChange$ = this.layoutSize$.pipe(
+    shareReplay({ refCount: true }),
+  );
+
+  changeLayoutSize() {
+    this.layoutSize$.next();
+  }
+
+  onChangeLayoutSize(): Observable<any> {
+    return this.layoutSizeChange$.pipe(delay(1));
+  }
+}
