046540cd786bf51849fd5e4e26199a88d4d7249c
sashaqred <sashaqred@ya.ru>
2020-04-15 12:03:52 +0300

    fix: replace change layout with safe change

--- src/app/pages/dashboard/electricity/electricity-chart/electricity-chart.component.ts
+++ src/app/pages/dashboard/electricity/electricity-chart/electricity-chart.component.ts
@@ -27,7 +27,7 @@ export class ElectricityChartComponent implements AfterViewInit, OnDestroy {
 
   constructor(private theme: NbThemeService,
               private layoutService: LayoutService) {
-    this.layoutService.onChangeLayoutSize()
+    this.layoutService.onSafeChangeLayoutSize()
       .pipe(
         takeWhile(() => this.alive),
       )
