20d7f7d0bd6174a76c7ff051b303d624214f78a0
Alex <alexey.dred@gmail.com>
2020-05-06 17:44:16 +0300

    fix(docs): remove unused script

--- scripts/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Iterates over all modules bundled in the src/.lib and publish them
-for dir in ./src/.lib/*/
-do
-    dir=${dir%*/}
-    npm publish --access=public src/.lib/${dir##*/}
-done
