cd85a480af1de870fc1879826667bcc804be42ea
Admin <alexey.dred@gmail.com>
2020-08-13 14:03:06 +0300

    feat(app): preparing for migration to angular 10

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
