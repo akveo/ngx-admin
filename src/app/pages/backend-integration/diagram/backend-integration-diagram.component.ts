cc937192d79597fc577eb2d2999d111062289b55
Sergey Filinsky <s.filinsky@akveo.com>
2020-09-16 22:18:58 +0300

    feat(demo): added backend integration descriptions

---
+++ src/app/pages/backend-integration/diagram/backend-integration-diagram.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-backend-integration-diagram',
+  templateUrl: './backend-integration-diagram.component.html',
+  styleUrls: ['./backend-integration-diagram.component.scss'],
+})
+export class BackendIntegrationDiagramComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
