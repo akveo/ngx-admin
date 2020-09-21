3d13bcb5099e53476c3cfa9841093c6c8cd951f4
Sergey Filinsky <s.filinsky@akveo.com>
2020-09-21 16:16:55 +0300

    feat(demo): cleared code

--- src/app/pages/backend-integration/integration-description/integration-description.component.ts
+++ src/app/pages/backend-integration/integration-description/integration-description.component.ts
@@ -1,20 +1,20 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'ngx-integration-description[features][url][buttonText]',
+  selector: 'ngx-integration-description',
   template: `
-    <div class='header'> For why do you need a backend admin dashboard?</div>
-    <div class='subheader'> To save up to 300 hours on development. To use backend as ready to use examples. </div>
-    <div class='header2'> Features </div>
-    <ul class='features-list'>
-      <li *ngFor='let feature of features'
-           class='feature'
+    <div class="header"> For why do you need a backend admin dashboard?</div>
+    <div class="subheader"> To save up to 300 hours on development. To use backend as ready to use examples. </div>
+    <div class="header2"> Features </div>
+    <ul class="features-list">
+      <li *ngFor="let feature of features"
+           class="feature"
       >
-        - {{ feature }}
+        {{ feature }}
       </li>
     </ul>
     <a nbButton
-       [href]='url'
+       [href]="url"
        status="primary"
        target="_blank"
     > {{buttonText}} </a>
@@ -26,7 +26,5 @@ export class IntegrationDescriptionComponent {
   @Input() features: string[];
   @Input() url: string;
   @Input() buttonText: string;
-
-  constructor() { }
 
 }
