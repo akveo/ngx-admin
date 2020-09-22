268e5cfb1ba0b92b6e665d71c576ce7fb66fe5f0
Sergey Filinsky <s.filinsky@akveo.com>
2020-09-22 17:18:47 +0300

    feat(demo): changed diagram to image feat(demo): improved styles

--- src/app/pages/backend-integration/integration-description/integration-description.component.ts
+++ src/app/pages/backend-integration/integration-description/integration-description.component.ts
@@ -3,9 +3,9 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'ngx-integration-description',
   template: `
-    <div class="header"> For why do you need a backend admin dashboard?</div>
-    <div class="subheader"> To save up to 300 hours on development. To use backend as ready to use examples. </div>
-    <div class="header2"> Features </div>
+    <div class="h6"> For why do you need a backend admin dashboard?</div>
+    <div class="subtitle"> To save up to 300 hours on development. To use backend as ready to use examples. </div>
+    <div class="h6"> Features </div>
     <ul class="features-list">
       <li *ngFor="let feature of features"
            class="feature"
