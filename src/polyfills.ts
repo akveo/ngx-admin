181331a894acb0f2ec66bfdfedb56c52aa6b4195
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2021-11-12 17:09:55 +0300

    feat: update Angular and Angular CDK packages

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -16,23 +16,12 @@
 
 /***************************************************************************************************
 * BROWSER POLYFILLS
-*/
-
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+*/  // Run `npm install --save classlist.js`.  // Run `npm install --save web-animations-js`.
 
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
-
-
-/** ALL Firefox browsers require the following to support `@angular/animation`. **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 /***************************************************************************************************
