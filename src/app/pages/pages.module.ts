e40fda67cd3bf37a1147628ada34cc903b759a97
Eduardo Silva <eduardo.lour.silva@gmail.com>
2022-05-11 09:16:11 -0300

    fix(nav): fix layout pages navigation

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   imports: [
@@ -16,6 +17,7 @@ import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
     DashboardModule,
     ECommerceModule,
     MiscellaneousModule,
+    LayoutModule,
   ],
   declarations: [
     PagesComponent,
