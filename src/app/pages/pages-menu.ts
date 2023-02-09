3d1d630d2e9bc5991c64b0a9f48e8098eab998d2
Israe Elibrahimi <50199415+israeElibr@users.noreply.github.com>
2023-02-09 17:03:59 +0100

    monDolibar

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,13 +1,72 @@
-import { NbMenuItem } from '@nebular/theme';
+import { NbMenuItem } from "@nebular/theme";
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'E-commerce',
-    icon: 'shopping-cart-outline',
-    link: '/pages/dashboard',
+    title: "E-commerce",
+    icon: "shopping-cart-outline",
+    link: "/pages/dashboard",
     home: true,
   },
-]
+  {
+    title: "Auth",
+    icon: "lock-outline",
+    children: [
+      {
+        title: "Login",
+        link: "/auth/login",
+      },
+      {
+        title: "Reset Password",
+        link: "/auth/reset-password",
+      },
+    ],
+  },
+  {
+    title: "Forms",
+    icon: "edit-2-outline",
+    children: [
+      {
+        title: "Form Inputs",
+        link: "/pages/forms/inputs",
+      },
+      {
+        title: "Form Layouts",
+        link: "/pages/forms/layouts",
+      },
+      {
+        title: "Buttons",
+        link: "/pages/forms/buttons",
+      },
+      {
+        title: "Datepicker",
+        link: "/pages/forms/datepicker",
+      },
+    ],
+  },
+  {
+    title: "UI Features",
+    icon: "keypad-outline",
+    link: "/pages/ui-features",
+    children: [
+      {
+        title: "Grid",
+        link: "/pages/ui-features/grid",
+      },
+      {
+        title: "Icons",
+        link: "/pages/ui-features/icons",
+      },
+      {
+        title: "Typography",
+        link: "/pages/ui-features/typography",
+      },
+      {
+        title: "Animated Searches",
+        link: "/pages/ui-features/search-fields",
+      },
+    ],
+  },
+];
 // export const MENU_ITEMS: NbMenuItem[] = [
 //   {
 //     title: 'E-commerce',
