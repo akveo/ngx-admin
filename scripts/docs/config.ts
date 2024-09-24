d354bd1e840e7c9a8b094151e9e12adaf82e8fe4
Denis Strigo <denis2295@gmail.com>
2024-09-24 18:32:04 +0200

    fix(demo-build): fix demo dosc deployments (#6043)

--- scripts/docs/config.ts
+++ scripts/docs/config.ts
@@ -2,3 +2,4 @@ export const REPO_URL = 'https://github.com/akveo/ngx-admin.git';
 export const REPO_OWNER = 'akveo';
 export const REPO_NAME = 'ngx-admin';
 export const OUT_DIR = 'docs/dist'; // Relative to the directory you run command
+export const GH_PAT = process.env.GH_PAT || ''; // Relative to the directory you run command
